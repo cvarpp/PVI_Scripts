34c42fa24390cb5ed37019a3e41ce58c9047e31f
MonahB01 <brian.monahan@mssm.edu>
2022-09-08 11:43:23 -0400

    Fixed Cam_convert
    made the Cam convert script run and work as intended

--- cam_convert.py
+++ cam_convert.py
@@ -4,43 +4,32 @@ Created on Thu May 26 11:11:43 2022
 
 @author: bmona
 """
+#%%
 import pandas as pd
 import util
 from datetime import date
 
 if __name__ == '__main__':
             
-    cam_archive = pd.read_excel(util.clin_ops + 'CAM Archive/CAM Archive.xlsx', sheet_name=None)
+    cam_archive = pd.read_excel(util.clin_ops + 'CAM Archive/CAM Archive.xlsx', sheet_name=None, header=None)
        
     shared_header = ['Date', 'Time', 'Coordinator Initials', 'Patient Name', 'Study',
         'Visit Type / Samples Needed', 'New or Follow-up?', 'Participant ID',
         'Sample ID', 'Processing location', 'Internal Notes']
     cam_arch = []    
     all_dfs = []
-    sheet_dfs = []
-    blank=[] 
-    y=0
-    
+    sheet_df_arch = []
+
+    print("Cam df size: {}".format(len(cam_archive)))
     for sname, df_week in cam_archive.items():
-        
-        # Returns a tuple of the dataset and compares position 0 of the tuple list to the #
-        
+                
         if df_week.shape[0] < 20:
             continue
-        elif df_week.shape[0] < 200:
-            df_week.columns = df_week.iloc[6, :]
-            # print(df_week.columns)
-        else:
-            df_week.columns = df_week.iloc[14, :]
-        # THE BIG CHEESE RIGHT HERE!
-        # generates dfs containted within "a call" (wrong word look up later) 
-        # check the numbered index location of the column adds 11 to it to form the range of the table (how wide the table of interest)
-        # for the col_num and col (in parallel) assign numbers to columns in the data frame. then selecting based on if the col is a string and Date is in the column name.
-        # only confusing part is the fact that you can do this all in one line
-        date_dfs = [df_week.iloc[:, col_num:col_num + 11] for col_num, col in enumerate(df_week.columns) if type(col) == str and 'Date' in col]
+        df_week.columns = df_week.iloc[6, :]
+
+        date_dfs = [df_week.iloc[:, col_num:col_num + 11] for col_num, col in enumerate(df_week.columns) if type(col) == str and 'date' in col.strip().lower()]
         
-        #for the df that is data dfs
-        
+     
         for df in date_dfs:
         
             df.columns = shared_header
@@ -49,72 +38,86 @@ if __name__ == '__main__':
             
             df = df[df['Date'] != 'Date']
             
-            sheet_dfs.append(df.dropna(subset=[sample_id_col]))
-                         
-    cam_arch = pd.concat(sheet_dfs).drop_duplicates()
+            sheet_df_arch.append(df.dropna(subset=[sample_id_col]))
+
+    cam_arch = pd.concat(sheet_df_arch)
+
+    cam_arch.drop_duplicates(inplace=True)
   
     cam_arch.to_excel(util.clin_ops + 'Long-Form CAM Archive.xlsx', index=False)
 
     print("Cam Archive Complete")
-
-    #where the script lives:    
-    
-    cam_active = pd.read_excel(util.clin_ops + 'CAM Clinic Schedule.xlsx', sheet_name=None)
-                  
-    shared_header = ['Date', 'Time', 'Coordinator Initials', 'Patient Name', 'Study',
+     
+    cam_active = pd.read_excel(util.clin_ops + 'CAM Clinic Schedule.xlsx', sheet_name=None, header=None)
+                   
+    shared_header_1 = ['Date', 'Time', 'Coordinator Initials', 'Patient Name', 'Study',
         'Visit Type / Samples Needed', 'New or Follow-up?', 'Participant ID',
         'Sample ID', 'Processing location', 'Internal Notes']
     
+    shared_header_2 = ['Date', 'Time', 'Patient Name', 'Study',
+        'Visit Type / Samples Needed', 'New or Follow-up?', 'Participant ID',
+        'Sample ID', 'Time collected', 'Phlebotomist', 'Coordinator Initials', 'Internal Notes']
+    
     all_dfs = []
-    sheet_dfs = []
+    sheet_df1 = []
+    sheet_df2 =[]
     blank=[]
 
     print("Cam df size: {}".format(len(cam_active)))
-    
+
     for sname, df_week in cam_active.items():
         
         if df_week.shape[0] < 20:
             continue
         elif df_week.shape[0] < 200:
             df_week.columns = df_week.iloc[6, :]
+            med=1
             # print(df_week.columns)
         else:
             df_week.columns = df_week.iloc[14, :]
+            med=0
 
-        # Returns a tuple of the dataset and compares position 0 of the tuple list to the #
+        if med == 1:
+            date_df1 = [df_week.iloc[:, col_num:col_num + 11] for col_num, col in enumerate(df_week.columns) if type(col) == str and 'date' in col.strip().lower()]
+        else:
+            date_df2 = [df_week.iloc[:, col_num:col_num + 12] for col_num, col in enumerate(df_week.columns) if type(col) == str and 'date' in col.strip().lower()]
         
-        # THE BIG CHEESE RIGHT HERE!
-        # generates dfs contained within "a call" (wrong word look up later) 
-        # check the numbered index location of the column adds 11 to it to form the range of the table (how wide the table of interest)
-        # for the col_num and col (in parallel) assign numbers to columns in the data frame. then selecting based on if the col is a string and Date is in the column name.
-        # only confusing part is the fact that you can do this all in one line
-        
-        date_dfs = [df_week.iloc[:, col_num:col_num + 11] for col_num, col in enumerate(df_week.columns) if type(col) == str and 'Date' in col]
-        #for the df that is data dfs
-        
-        for df in date_dfs:
+        if med == 1:
+            for df in date_df1:
+            
+                df.columns = shared_header_1
+                        
+                sample_id_col = [val for val in df.columns if 'Sample ID' in val][0]
+            
+                df = df[df['Date'] != 'Date']
+            
+                sheet_df1.append(df.dropna(subset=[sample_id_col]))
 
-            #set column headers to the common header list
-            df.columns = shared_header
+        elif med == 0:
+            for df in date_df2:
             
-            #val must be a common value function.
-            #bruh he has val 3 times why?
+                df.columns = shared_header_2
             
-            sample_id_col = [val for val in df.columns if 'Sample ID' in val][0]
+                sample_id_col = [val for val in df.columns if 'Sample ID' in val][0]
             
-            df = df[df['Date'] != 'Date']
+                df = df[df['Date'] != 'Date']
             
-            sheet_dfs.append(df.dropna(subset=[sample_id_col]))
+                sheet_df2.append(df.dropna(subset=[sample_id_col]))
+   
+    ca1 = pd.concat(sheet_df1)
 
-    cam_act = pd.concat(sheet_dfs).drop_duplicates()
+    ca2 = pd.concat(sheet_df2)  
+
+    cam_act = pd.concat([ca1, ca2])
+
+    cam_act.drop_duplicates(inplace=True)
   
     cam_act.to_excel(util.clin_ops + 'Long-Form CAM active.xlsx', index=False)
 
-    print("Cam Active complete")
+    cam_both = pd.concat([cam_act, cam_arch])
 
-    cam_both = pd.concat([cam_arch,cam_act]).drop_duplicates()
+    cam_both.drop_duplicates(inplace=True)
 
-    cam_both.to_excel(util.clin_ops + 'Long-Form CAM_both {}.xlsx'.format(date.today().strftime("%m.%d.%y"), index=False))
+    cam_both.to_excel(util.clin_ops + "Long form Cam schedule.xlsx", index=False)
 
-    print("Cam Calenders Compiled")
-    print('filepath:Clinical Research Study Operations/Long-Form CAM_both {}'.format(date.today().strftime("%m.%d.%y")))
+    print("done")
