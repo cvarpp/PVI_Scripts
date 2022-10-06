4ce78037b67316fa3d46c3ce036cedba6774a5f5
Charles Gleason <36055314+crgl@users.noreply.github.com>
2022-10-06 16:45:44 -0400

    Add argument parsing to speed up regular use
    Cut runtime in half when not updating archived visits

--- cam_convert.py
+++ cam_convert.py
@@ -8,11 +8,12 @@ Created on Thu May 26 11:11:43 2022
 import pandas as pd
 import util
 import datetime
+import argparse
 
 def clean_date(s):
     return pd.to_datetime(s.apply(lambda val: val if isinstance(val, datetime.datetime) else '1/1/1900')).dt.date
 
-if __name__ == '__main__':
+def pull_archive(output_fname=util.clin_ops + 'CAM Archive/Archive Long.xlsx'):
     cam_archive = pd.read_excel(util.clin_ops + 'CAM Archive/CAM Archive.xlsx', sheet_name=None, header=None)
 
     sheet_df_arch = []
@@ -20,7 +21,7 @@ if __name__ == '__main__':
     shared_header = ['Date', 'Time', 'Time collected', 'Patient Name', 'Study',
         'Visit Type / Samples Needed', 'New or Follow-up?', 'Participant ID',
         'Sample ID', 'Visit Coordinator', 'Internal Notes']
-    for sname, df_week in cam_archive.items():
+    for _sname, df_week in cam_archive.items():
         if df_week.shape[0] < 20:
             continue
         df_week.columns = df_week.iloc[6, :]
@@ -29,10 +30,22 @@ if __name__ == '__main__':
             df.columns = shared_header
             df = df[df['Date'] != 'Date']
             sheet_df_arch.append(df.dropna(subset=['Sample ID']))
-    cam_arch = pd.concat(sheet_df_arch).drop_duplicates()
+    idx_cols = ['Date', 'Time', 'Participant ID', 'Sample ID']
+    cam_arch = pd.concat(sheet_df_arch).drop_duplicates(subset=idx_cols)
+    cam_arch.to_excel(output_fname, index=False)
+    return cam_arch
+
+if __name__ == '__main__':
+    argParser = argparse.ArgumentParser(description='Make Seronet monthly sample report.')
+    argParser.add_argument('-u', '--update', action='store_true')
+    args = argParser.parse_args()
+    long_archive = util.clin_ops + 'CAM Archive/Archive Long.xlsx'
+    if args.update:
+        cam_arch = pull_archive(output_fname=long_archive)
+    else:
+        cam_arch = pd.read_excel(long_archive)
 
     cam_active = pd.read_excel(util.clin_ops + 'CAM Clinic Schedule.xlsx', sheet_name=None, header=None)
-
     shared_header_1 = ['Date', 'Time', 'Time collected', 'Patient Name', 'Study',
         'Visit Type / Samples Needed', 'New or Follow-up?', 'Participant ID',
         'Sample ID', 'Visit Coordinator', 'Internal Notes']
@@ -41,8 +54,7 @@ if __name__ == '__main__':
         'Sample ID', 'Time Collected', 'Phlebotomist', 'Visit Coordinator', 'Internal Notes']
 
     sheet_dfs = []
-
-    for sname, df_week in cam_active.items():
+    for _sname, df_week in cam_active.items():
         if df_week.shape[0] < 20:
             continue
         elif df_week.shape[0] < 200:
@@ -57,8 +69,14 @@ if __name__ == '__main__':
             sheet_dfs.append(date_df)
 
     exclude = set(['Participant ID', 'Lunch Break', 'BLOCK'])
-    cam_act = pd.concat(sheet_dfs).dropna(subset=['Participant ID']).drop_duplicates()
-    cam_both = pd.concat([cam_act, cam_arch]).drop_duplicates().query('`Participant ID` not in @exclude').assign(Date = lambda df: clean_date(df['Date'])).sort_values(by='Date', ascending=False)
+    cam_act = pd.concat(sheet_dfs).dropna(subset=['Participant ID'])
+    idx_cols = ['Date', 'Time', 'Participant ID', 'Sample ID']
+    cam_both = (pd.concat([cam_act, cam_arch]).drop_duplicates(subset=idx_cols)
+                    .query('`Participant ID` not in @exclude')
+                    .assign(Date = lambda df: clean_date(df['Date']),
+                            idx=lambda df: df.loc[:, idx_cols].astype(str).sum(axis=1))
+                    .sort_values(by='Date', ascending=False)
+                    .set_index('idx'))
 
     output_fname = util.clin_ops + "Long-Form CAM Schedule.xlsx"
     cam_both.to_excel(output_fname, index=False)
