3fc34348b2003bb542cdf59d61d4117a9f9e721d
Charles Gleason <36055314+crgl@users.noreply.github.com>
2022-11-01 10:25:53 -0400

    Update csv conversion script to use argparse

--- convert_to_csvs.py
+++ convert_to_csvs.py
@@ -2,15 +2,15 @@ import pandas as pd
 import sys
 import glob
 import os
+import argparse
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("usage: python convert_to_csvs.py input_directory output_directory")
-        exit(1)
-    input_dirname = sys.argv[1]
-    output_dirname = sys.argv[2]
-    for fname in glob.glob('{}/*xls*'.format(input_dirname)):
+    argParser = argparse.ArgumentParser(description='Read Excel files in input_dir and create corresponding csv files in output_dir')
+    argParser.add_argument('-i', '--input_dir', action='store', required=True)
+    argParser.add_argument('-o', '--output_dir', action='store', required=True)
+    args = argParser.parse_args()
+    for fname in glob.glob('{}/*xls*'.format(args.input_dir)):
         print("Converting", fname)
-        pd.read_excel(fname, na_filter=False, keep_default_na=False).to_csv('{}/{}.csv'.format(output_dirname, fname.split(os.sep)[-1][:-5]), index=False)
+        pd.read_excel(fname, na_filter=False, keep_default_na=False).to_csv('{}/{}.csv'.format(args.output_dir, fname.split(os.sep)[-1].split('.')[0]), index=False)
         print(fname, "converted!")
     print("Done!")
