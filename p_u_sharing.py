3dd3f679d7e586c2a9afc9f6a57f01e1b0385bd9
jdm782 <107207518+jdm782@users.noreply.github.com>
2023-03-30 11:13:25 -0400

    Update p_u_sharing.py
    result sharing now pulls matching emails for visits. seems to pull dates further back than intended, still

--- p_u_sharing.py
+++ p_u_sharing.py
@@ -7,7 +7,7 @@ import datetime
 from dateutil import parser
 
 if __name__ == '__main__':
-    paris_info = pd.read_excel(util.paris_tracker , sheet_name='Main', header=8)
+    paris_info = pd.read_excel(util.paris_tracker, sheet_name='Main', header=8)
     umbrella_info = pd.read_excel(util.umbrella_tracker, sheet_name='Summary', header=0)
     samples = pd.read_excel(util.intake, sheet_name='Sample Intake Log', header=6, keep_default_na=False)
     visit_type = "Visit Type / Samples Needed"
@@ -16,20 +16,21 @@ if __name__ == '__main__':
     result_value_col = 'Ab Concentration (Units - AU/mL)'
     was_shared_col = 'Clinical Ab Result Shared?'
     result_type = "Visit Type / Samples Needed"
+    pemail = paris_info[['Subject ID', 'E-mail']]
+    uemail = umbrella_info[['Subject ID', 'Email']]
     samplesClean = samples.dropna(subset=['Participant ID'])
     participants = [str(x).strip().upper() for x in pd.read_excel(util.intake, sheet_name='Sample Intake Log', header=6)['Participant ID']]
     def not_shared(val):
-        return pd.isna(val) or val == 'No' or val == ''    
-    
+        return pd.isna(val) or val == 'No' or val == ''
+
     share_filter = samples[was_shared_col].apply(not_shared)
     ppl_filter = samples['Participant ID'] != ''
     result_filter = (samples[result_value_col] != '') & (samples[result_value_col] != 'N/A')
-    #merged_emails = pd.merge(paris_info, umbrella_info, on='Participant ID', how='inner')
-    #matching_email = merged_emails.loc[merged_emails['Participant ID'] == 'Participant ID']
+    emails = pd.merge(pemail, uemail, on='Subject ID', how='outer')
+    emails['Email'] = emails['Email'].fillna(emails['E-mail'])
+    emails = emails[['Subject ID', 'Email']]
 
-    drop_cols = ['Participant ID', result_value_col]    
-
-    samplesClean = samples[share_filter & ppl_filter & result_filter].dropna(subset=drop_cols)
+    samplesClean = samples[share_filter & ppl_filter & result_filter].dropna(subset=['Participant ID', result_value_col])
     participants = [str(x).strip().upper() for x in samplesClean['Participant ID'].unique()]
     participant_results = {participant: [] for participant in participants}
     for _, sample in samplesClean.iterrows():
@@ -59,38 +60,30 @@ if __name__ == '__main__':
                     except:
                         print("Sample", sample['Sample_ID'], "improperly recorded as collected on", sample['Date Collected'])
                         print("Not included in result sharing")
-    data = {'Participant ID': [], 'Date': [], 'Sample ID': [], 'Visit Type': [], 'Qualitative': [], 'Quantitative': []}
+    data_filtered = {'Participant ID': [], 'Date': [], 'Email': [], 'Visit Type': [], 'Qualitative': [], 'Quantitative': []}
+    today = date.today()
+    last_60_days = today - datetime.timedelta(days=60)
     for participant, results in participant_results.items():
-       # try:
-        #    samples.sort(key=lambda x: x[0])
-         #   results.sort(key=lambda x: x[0])
-        #except:
-         #   print(participant)
-          #  print(samples)
-           # print(results)
-            #continue
-        if len(samples) < 1:
-            if len(results) < 1:
-                continue
-        #baseline = samples[0][0]
-        for date_, sample_id, visit_type, result, result_new in results:
-            sample_id = str(sample_id).strip()
-            data['Participant ID'].append(participant)
-            data['Date'].append(date_)
-            data['Qualitative'].append(result)
-            data['Quantitative'].append(result_new)
+        for result in results:
+            if result[0] >= last_60_days:
+                for date_collected, sample_id, visit_type, result_stat, result_value in participant_results[participant]:
+                    data_filtered['Participant ID'].append(participant)
+                    data_filtered['Date'].append(date_collected)
+                    data_filtered['Visit Type'].append(visit_type)
+                    data_filtered['Qualitative'].append(result_stat)
+                    data_filtered['Quantitative'].append(result_value)
+                    if not emails.loc[emails['Subject ID'] == participant].empty:
+                        email = emails.loc[emails['Subject ID'] == participant, 'Email'].iloc[0]
+                        data_filtered['Email'].append(email)
+                    else:
+                        data_filtered['Email'].append('')
 if __name__ == '__main__':
-    report = pd.DataFrame(data)
-    output_filename = util.script_output + 'result_reporting_test_{}.xlsx'.format(date.today().strftime("%m.%d.%y"))
-    report.to_excel(output_filename, index=False)
-    cutoff_date = datetime.date.today() - datetime.timedelta(days=60)
-    old_results = report[report['Date'] <= cutoff_date]
-    new_results = report[report['Date'] > cutoff_date]
-    assert old_results.shape[0] + new_results.shape[0] == report.shape[0]
+    print(data_filtered)
+    report = pd.DataFrame(data_filtered)
+    report.columns = ['Participant ID', 'Date', 'Email', 'Visit Type / Sample ID', 'Qualitative', 'Quantitative']
     output_filename = util.script_output + 'result_reporting_test_{}.xlsx'.format(date.today().strftime("%m.%d.%y"))
     writer = pd.ExcelWriter(output_filename)
-    new_results.to_excel(writer, sheet_name='To Share')
-    old_results.to_excel(writer, sheet_name='Misunderstood')
+    report.to_excel(writer, index=False)
     writer.save()
     writer.close()
     print("Report written to {}".format(output_filename))
