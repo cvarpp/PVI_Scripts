f3a032c14d79c2719376e73d2d9d1eb511dd1d39
Charles Gleason <36055314+crgl@users.noreply.github.com>
2023-03-31 11:47:51 -0400

    Use helper function for intake log pull
    This involves a fair number of changes (and a couple of blocks moved for clarity), but is really just as it says in the commit.
    Right now your participant list is being derived from the sample intake log, but you could pull it from the paris & umbrella trackers and use it to filter your initial intake log query.
    For now, I haven't made any changes like that. I was able to remove a lot of the filtering and the result cleaning since that happens in the query_intake function. You'll also see a couple of column name changes. Possible that you were not using this because it initially dropped the sample sharing columns, but don't hesitate to make small edits to helpers.py if they simplify your code (as they do in this case).

--- p_u_sharing.py
+++ p_u_sharing.py
@@ -5,57 +5,44 @@ import util
 from datetime import date
 import datetime
 from dateutil import parser
+from helpers import query_intake
+
 
 if __name__ == '__main__':
     paris_info = pd.read_excel(util.paris_tracker, sheet_name='Main', header=8)
     umbrella_info = pd.read_excel(util.umbrella_tracker, sheet_name='Summary', header=0)
-    samples = pd.read_excel(util.intake, sheet_name='Sample Intake Log', header=6)
-    visit_type = "Visit Type / Samples Needed"
     results_recieved = 'Results received' # never used
-    result_stat_col = 'Ab Detection S/P Result (Clinical) (Titer or Neg)'
-    result_value_col = 'Ab Concentration (Units - AU/mL)'
     was_shared_col = 'Clinical Ab Result Shared?'
-    pemail = paris_info[['Subject ID', 'E-mail']].rename(columns={'E-mail': 'Email'})
-    uemail = umbrella_info[['Subject ID', 'Email']]
-    samplesClean = samples.dropna(subset=['Participant ID'])
-    participants = [str(x).strip().upper() for x in pd.read_excel(util.intake, sheet_name='Sample Intake Log', header=6)['Participant ID']]
     def not_shared(val):
         return pd.isna(val) or val == 'No' or val == ''
 
+    samples = query_intake()
     share_filter = samples[was_shared_col].apply(not_shared)
-    ppl_filter = samples['Participant ID'] != ''
-    result_filter = (samples[result_value_col] != '') & ~samples[result_value_col].apply(pd.isna)
+    result_filter = (samples['Qualitative'] != '') & ~samples['Qualitative'].apply(pd.isna)
+    samplesClean = samples[share_filter & result_filter].copy()
+
+    pemail = paris_info[['Subject ID', 'E-mail']].rename(columns={'E-mail': 'Email'})
+    uemail = umbrella_info[['Subject ID', 'Email']]
     emails = pd.concat([pemail, uemail])
 
-    samplesClean = samples[share_filter & ppl_filter & result_filter].dropna(subset=['Participant ID', result_value_col])
-    participants = [str(x).strip().upper() for x in samplesClean['Participant ID'].unique()]
+    participants = samplesClean['participant_id'].unique()
     participant_results = {participant: [] for participant in participants}
-    for _, sample in samplesClean.iterrows():
-        participant = str(sample['Participant ID']).strip().upper()
+    for sid, sample in samplesClean.iterrows():
+        participant = sample['participant_id']
         if participant in participant_results.keys():
-            if str(sample[result_stat_col]).strip().upper() == "NEGATIVE":
-                sample[result_value_col] = "Negative"
-            if pd.isna(sample[result_value_col]):
-                result_new = '-'
-            elif type(sample[result_value_col]) == int:
-                result_new = sample[result_value_col]
-            elif str(sample[result_value_col]).strip().upper() == "NEGATIVE":
-                result_new = 1.
-            else:
-                result_new = sample[result_value_col]
             try:
-                participant_results[participant].append((parser.parse(str(sample['Date Collected'])).date(), sample['Sample ID'], sample[visit_type], sample[result_stat_col], result_new))
+                participant_results[participant].append((parser.parse(str(sample['Date Collected'])).date(), sid, sample[util.visit_type], sample['Qualitative'], sample['Quantitative']))
             except:
                 print(sample['Date Collected'])
-                participant_results[participant].append((parser.parse('1/1/1900').date(), sample['Sample ID'], sample[visit_type], sample[result_stat_col], result_new))
+                participant_results[participant].append((parser.parse('1/1/1900').date(), sid, sample[util.visit_type], sample['Qualitative'], sample['Quantitative']))
         if pd.isna(sample[was_shared_col]) or sample[was_shared_col] == "No":
             if participant in participant_results.keys():
-                    if str(sample[result_stat_col]).strip().upper() == "NEGATIVE":
-                        sample[result_value_col] = "Negative"
+                    if str(sample['Qualitative']).strip().upper() == "NEGATIVE":
+                        sample['Quantitative'] = "Negative"
                     try:
-                        participant_results[participant].append((parser.parse(str(sample['Date Collected'])).date(), sample['Sample ID'], sample[visit_type], sample[result_stat_col], sample[result_value_col]))
+                        participant_results[participant].append((parser.parse(str(sample['Date Collected'])).date(), sample['Sample ID'], sample[util.visit_type], sample['Qualitative'], sample['Quantitative']))
                     except:
-                        print("Sample", sample['Sample_ID'], "improperly recorded as collected on", sample['Date Collected'])
+                        print("Sample", sid, "improperly recorded as collected on", sample['Date Collected'])
                         print("Not included in result sharing")
     data_filtered = {'Participant ID': [], 'Sample ID': [], 'Date': [], 'Email': [], 'Visit Type': [], 'Qualitative': [], 'Quantitative': []}
     today = date.today()
@@ -63,11 +50,11 @@ if __name__ == '__main__':
     for participant, results in participant_results.items():
         for result in results:
             if result[0] >= last_60_days:
-                for date_collected, sample_id, visit_type, result_stat, result_value in participant_results[participant]:
+                for date_collected, sample_id, util.visit_type, result_stat, result_value in participant_results[participant]:
                     data_filtered['Participant ID'].append(participant)
                     data_filtered['Sample ID'].append(sample_id)
                     data_filtered['Date'].append(date_collected)
-                    data_filtered['Visit Type'].append(visit_type)
+                    data_filtered['Visit Type'].append(util.visit_type)
                     data_filtered['Qualitative'].append(result_stat)
                     data_filtered['Quantitative'].append(result_value)
                     if not emails.loc[emails['Subject ID'] == participant].empty:
