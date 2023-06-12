ef79af10df0ca1ea4607d95c7739444170d6c0ba
Charles Gleason <36055314+crgl@users.noreply.github.com>
2023-06-12 14:48:10 -0400

    Restructure par_umb_sharing with argparse

--- par_umb_sharing.py
+++ par_umb_sharing.py
@@ -6,29 +6,24 @@ from datetime import date
 import datetime
 from dateutil import parser
 from helpers import query_intake
+import argparse
 
+def not_shared(val):
+    return pd.isna(val) or val == 'No' or val == ''
 
-if __name__ == '__main__':
+def make_report(use_cache=False):
     paris_info = pd.read_excel(util.paris_tracker, sheet_name='Main', header=8)
     umbrella_info = pd.read_excel(util.umbrella_tracker, sheet_name='Summary', header=0)
     was_shared_col = 'Clinical Ab Result Shared?'
-    def not_shared(val):
-        return pd.isna(val) or val == 'No' or val == ''
 
-    today = date.today()
-    samples = query_intake()
+    samples = query_intake(use_cache=use_cache)
     share_filter = samples[was_shared_col].apply(not_shared)
     result_filter = (samples['Qualitative'] != '') & ~samples['Qualitative'].apply(pd.isna)
     samplesClean = samples[share_filter & result_filter].copy()
     samplesCleanAll = samplesClean.copy()
-    #samplesClean = samplesClean[samplesClean['Date Collected'] >= (today - datetime.timedelta(days=60))]
-    
-    # older_results = samplesClean[samplesClean['Date Collected'] < (today - datetime.timedelta(days=60))]
     older_results = []
-    #df for results older than 60 days
     pemail = paris_info[['Subject ID', 'E-mail']].rename(columns={'E-mail': 'Email'})
     uemail = umbrella_info[['Subject ID', 'Email']]
-    # emails = pd.concat([pemail, uemail])
     emails = (pd.concat([pemail, uemail])
                 .assign(pid=lambda df: df['Subject ID'].str.strip())
                 .drop_duplicates(subset='pid').set_index('pid'))
@@ -46,34 +41,38 @@ if __name__ == '__main__':
             except:
                 print("Sample", sid, "improperly recorded as collected on", sample['Date Collected'])
                 print("Not included in result sharing")
-    data_filtered = {'Participant ID': [], 'Sample ID': [], 'Date': [], 'Email': [], 'Visit Type': [], 'Qualitative': [], 'Quantitative': []}
-    last_60_days = today - datetime.timedelta(days=60)
+    data = {'Participant ID': [], 'Sample ID': [], 'Date': [], 'Email': [], 'Visit Type': [], 'Qualitative': [], 'Quantitative': []}
     for participant, results in participant_results.items():
         for result in results:
             date_collected, sample_id, util.visit_type, result_stat, result_value = result
-            if date_collected < last_60_days:
-                older_results.append({'Participant ID': participant, 'Sample ID': sample_id, 'Date': date_collected, 'Visit Type': util.visit_type,'Qualitative': result_stat,'Quantitative': result_value})
-                #add older results to 'older_results' df
-                continue
-            if date_collected >= last_60_days:
-                data_filtered['Participant ID'].append(participant)
-                data_filtered['Sample ID'].append(sample_id)
-                data_filtered['Date'].append(date_collected)
-                data_filtered['Visit Type'].append(util.visit_type)
-                data_filtered['Qualitative'].append(result_stat)
-                data_filtered['Quantitative'].append(result_value)
-                if participant in emails.index:
-                    email = emails.loc[participant, 'Email']
-                else:
-                    email = ''
-                data_filtered['Email'].append(email)
+            data['Participant ID'].append(participant)
+            data['Sample ID'].append(sample_id)
+            data['Date'].append(date_collected)
+            data['Visit Type'].append(util.visit_type)
+            data['Qualitative'].append(result_stat)
+            data['Quantitative'].append(result_value)
+            if participant in emails.index:
+                email = emails.loc[participant, 'Email']
+            else:
+                email = ''
+            data['Email'].append(email)
+    report = pd.DataFrame(data)
+    return report
+
 if __name__ == '__main__':
-    report = pd.DataFrame(data_filtered)
-    old_report = pd.DataFrame(older_results)
-    report.columns = ['Participant ID', 'Sample ID', 'Date', 'Email', 'Visit Type / Sample ID', 'Qualitative', 'Quantitative']
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-d', '--debug', action='store_true')
+    argparser.add_argument('-c', '--use_cache', action='store_true')
+    args = argparser.parse_args()
+
+    report = make_report(args.use_cache)
+    last_60_days = date.today() - datetime.timedelta(days=60)
+    old_report = report[report['Date'] < last_60_days]
+    report_filtered = report[report['Date'] >= last_60_days]
     output_filename = util.sharing + 'result_reporting_test_{}.xlsx'.format(date.today().strftime("%m.%d.%y"))
-    with pd.ExcelWriter(output_filename) as writer:
-        report.to_excel(writer, sheet_name='Needs Results - Recent', index=False)
-        old_report.to_excel(writer, sheet_name='Needs Results - Old(60d+)', index=False)
-        #make new sheet
-    print("Report written to {}".format(output_filename)) 
+    if not args.debug:
+        with pd.ExcelWriter(output_filename) as writer:
+            report_filtered.to_excel(writer, sheet_name='Needs Results - Recent', index=False)
+            old_report.to_excel(writer, sheet_name='Needs Results - Old(60d+)', index=False)
+        print("Report written to {}".format(output_filename))
+    print(report.shape[0], old_report.shape[0], report_filtered.shape[0])
