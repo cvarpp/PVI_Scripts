577e10516a401cef35edb43deabfb6b4df220288
Y.C <yxchen17@hotmail.com>
2023-07-06 12:24:47 -0400

    Draft for separate TITAN by old and new
    Just a draft, the idea is to separate TITAN results into 2 sheets. May not need be this complicated.

--- par_umb_sharing.py
+++ par_umb_sharing.py
@@ -29,8 +29,8 @@ def make_report(use_cache=False):
     pemail = paris_info[['Subject ID', 'E-mail']].rename(columns={'E-mail': 'Email'})
     uemail = umbrella_info[['Subject ID', 'Email']]
     temail = titan_info[['Umbrella Corresponding Participant ID', 'Email (From EPIC)']].rename(columns={'Umbrella Corresponding Participant ID': 'Subject ID', 'Email (From EPIC)': 'Email'})
-    uemail_without_temail = umbrella_info[['Subject ID', 'Email']][~umbrella_info['Subject ID'].isin(temail['Subject ID'])]
-    emails = (pd.concat([pemail, uemail_without_temail])
+    # uemail_without_temail = umbrella_info[['Subject ID', 'Email']][~umbrella_info['Subject ID'].isin(temail['Subject ID'])]
+    emails = (pd.concat([pemail, uemail, temail])
                 .assign(pid=lambda df: df['Subject ID'].str.strip())
                 .drop_duplicates(subset='pid').set_index('pid'))
 
@@ -53,6 +53,12 @@ if __name__ == '__main__':
     report_new = report[report['Date Collected'] >= recency_cutoff]
     report_new_emails = report_new[~report_new['Email'].isna()]
     report_new_no_emails = report_new[report_new['Email'].isna()]
+
+
+    temail_old = report_old[report_old['participant_id'].isin(temail['Subject ID'])]
+    temail_new = report_new[report_new['participant_id'].isin(temail['Subject ID'])]
+
+
     assert(report_old.shape[0] + report_new.shape[0] == report.shape[0])
     assert(report_new_emails.shape[0] + report_new_no_emails.shape[0] == report_new.shape[0])
     output_filename = util.sharing + 'result_reporting_{}.xlsx'.format(date.today().strftime("%m.%d.%y"))
@@ -60,6 +66,10 @@ if __name__ == '__main__':
         with pd.ExcelWriter(output_filename) as writer:
             report_new_emails.to_excel(writer, sheet_name='Results to Share', index=False)
             report_new_no_emails.to_excel(writer, sheet_name='Recent Unshared - Email Missing', index=False)
+            
+            temail_old.to_excel(writer, sheet_name='TITAN Unshared Old', index=False)
+            temail_new.to_excel(writer, sheet_name='TITAN Unshared New', index=False)
+            
             report_old.to_excel(writer, sheet_name='Older Unshared Results', index=False)
         print("Report written to {}".format(output_filename))
     print("Total to report:", report.shape[0], "Older count:", report_old.shape[0], "Recent Results to Share:", report_new_emails.shape[0], "Recent Results Missing Email:", report_new_no_emails.shape[0])
