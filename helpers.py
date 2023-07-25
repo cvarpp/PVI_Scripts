d3cff346fdd1b96b398dabefef3be7d028ad9479
Charles Gleason <36055314+crgl@users.noreply.github.com>
2023-07-24 20:46:22 -0400

    Update time columns in query_dscf

--- helpers.py
+++ helpers.py
@@ -135,8 +135,8 @@ def query_dscf(sid_list=None, no_pbmcs=set(), use_cache=False):
                     'Total Serum Vol. (mL)': 'Total volume of serum (mL)',
                     'SST Volume': 'SST VOL',
                     'Cell Tube Volume (mL)': 'CPT/EDTA VOL',
-                    'Date Processed': 'Date Processing Started',
-                    'Processor Initials': 'Processed by (initials)'} # This is a two-step transformation, but the end product is cleaner
+                    'Time in -80C (Serum)': 'Time put in -80: SERUM',
+                    'Time in Freezing Device': 'Time put in -80: PBMC',}
         new_samples = pd.read_excel(util.proc + 'Processing Notebook.xlsx', sheet_name='Specimen Dashboard', header=1).rename(columns=correct_new)
         all_samples = (pd.concat([bsl2p_archive, bsl2_archive, bsl2p_samples, bsl2_samples, crp_samples, new_samples])
                         .assign(sample_id=clean_sample_id)
