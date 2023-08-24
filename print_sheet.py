97318cde402a0602656d25129ea1faad2ad5952a
Y.C <yxchen17@hotmail.com>
2023-08-24 15:06:39 -0400

    Fix column index issue

--- print_sheet.py
+++ print_sheet.py
@@ -65,66 +65,66 @@ def generate_workbook(assigned_sample_ids, box_start, box_end, sheet_name, templ
             
             if print_type == 'SERONET':
                 if 'READ ME' in sheet_name:
-                    # box_numbers_range = range(box_start, box_end + 1)
-                    # sheet_data.loc[1:6, 'H'] = box_numbers_range
-                    # sheet_data.loc[7:24, 'H'] = assigned_sample_ids
-                    sheet_data.loc[2:19, 'M'] = assigned_sample_ids
+                    box_numbers_range = range(box_start, box_end + 1)
+                    sheet_data.loc[1:6, 7] = box_numbers_range
+                    sheet_data.loc[7:24, 7] = assigned_sample_ids
+                    sheet_data.loc[2:19, 12] = assigned_sample_ids
                     box_numbers_repeated = [box for box in range(box_start, box_end + 1) for _ in range(3)]
-                    sheet_data.loc[2:19, 'N'] = box_numbers_repeated
+                    sheet_data.loc[2:19, 13] = box_numbers_repeated
                 if '4.5 mL Tops' in sheet_name:
-                    sheet_data.loc[0:17, 'C'] = assigned_sample_ids
+                    sheet_data.loc[0:17, 2] = assigned_sample_ids
                 if '4.5 mL Sides' in sheet_name:
-                    sheet_data.loc[0:17, 'B'] = assigned_sample_ids_repeated
+                    sheet_data.loc[0:17, 1] = assigned_sample_ids_repeated
                 if '1-Box Top round1' in sheet_name:
                     assigned_sample_ids_1 = assigned_sample_ids[:6]
-                    sheet_data.loc[1:6, 'H'] = assigned_sample_ids_1
+                    sheet_data.loc[1:6, 7] = assigned_sample_ids_1
                     assigned_sample_ids_1_repeated = [sid for sid in assigned_sample_ids_1 for _ in range(15)]
-                    sheet_data.loc[1:90, 'B'] = assigned_sample_ids_1_repeated
+                    sheet_data.loc[1:90, 1] = assigned_sample_ids_1_repeated
                 if '2-Box Side round1' in sheet_name:
-                    sheet_data.loc[1:6, 'H'] = assigned_sample_ids_1
-                    sheet_data.loc[1:90, 'L'] = assigned_sample_ids_1_repeated
-                    sheet_data['B'] = sheet_data['M'] + " " + sheet_data['L']
+                    sheet_data.loc[1:6, 7] = assigned_sample_ids_1
+                    sheet_data.loc[1:90, 11] = assigned_sample_ids_1_repeated
+                    sheet_data[1] = sheet_data[12] + " " + sheet_data['L']
                 if '3-Box Top round2' in sheet_name:
                     assigned_sample_ids_2 = assigned_sample_ids[6:12]
-                    sheet_data.loc[1:6, 'H'] = assigned_sample_ids_2
+                    sheet_data.loc[1:6, 7] = assigned_sample_ids_2
                     assigned_sample_ids_2_repeated = [sid for sid in assigned_sample_ids_2 for _ in range(15)]
-                    sheet_data.loc[1:90, 'B'] = assigned_sample_ids_2_repeated
+                    sheet_data.loc[1:90, 1] = assigned_sample_ids_2_repeated
                 if '4-Box Side round2' in sheet_name:
-                    sheet_data.loc[1:6, 'H'] = assigned_sample_ids_2
-                    sheet_data.loc[1:90, 'L'] = assigned_sample_ids_2_repeated
-                    sheet_data['B'] = sheet_data['M'] + " " + sheet_data['L']
+                    sheet_data.loc[1:6, 7] = assigned_sample_ids_2
+                    sheet_data.loc[1:90, 11] = assigned_sample_ids_2_repeated
+                    sheet_data[1] = sheet_data[12] + " " + sheet_data['L']
                 if '5-Box Top round3' in sheet_name:
                     assigned_sample_ids_3 = assigned_sample_ids[12:18]
-                    sheet_data.loc[1:6, 'H'] = assigned_sample_ids_3
+                    sheet_data.loc[1:6, 7] = assigned_sample_ids_3
                     assigned_sample_ids_3_repeated = [sid for sid in assigned_sample_ids_3 for _ in range(15)]
-                    sheet_data.loc[1:90, 'B'] = assigned_sample_ids_3_repeated
+                    sheet_data.loc[1:90, 1] = assigned_sample_ids_3_repeated
                 if '6-Box Side round3' in sheet_name:
-                    sheet_data.loc[1:6, 'H'] = assigned_sample_ids_3
-                    sheet_data.loc[1:90, 'L'] = assigned_sample_ids_3_repeated
-                    sheet_data['B'] = sheet_data['M'] + " " + sheet_data['L']
+                    sheet_data.loc[1:6, 7] = assigned_sample_ids_3
+                    sheet_data.loc[1:90, 11] = assigned_sample_ids_3_repeated
+                    sheet_data[1] = sheet_data[12] + " " + sheet_data['L']
                 if '7-Kits' in sheet_name:
                     # assigned_sample_ids_repeated_total = []
                     # for i in range(0, len(assigned_sample_ids), 2):
                     #     assigned_sample_ids_repeated_total.extend([assigned_sample_ids[i], assigned_sample_ids[i + 1]] * 5)
                     assigned_sample_ids_repeated_total = [sid for i in range(0, len(assigned_sample_ids), 2) for sid in [assigned_sample_ids[i], assigned_sample_ids[i + 1]] * 5]
-                    sheet_data.loc[0:89, 'B'] = assigned_sample_ids_repeated_total
+                    sheet_data.loc[0:89, 1] = assigned_sample_ids_repeated_total
 
             if print_type == 'SERUM':
                 if sheet_name == '1 - Kits':
                     box_numbers_repeated = [box for box in range(box_start, box_end + 1) for _ in range(6)]
                     assigned_sample_ids_repeated = [sid for sid in assigned_sample_ids for _ in range(2)]
-                    sheet_data.loc[1:24, 'N'] = assigned_sample_ids
-                    sheet_data.loc[1:24, 'O'] = box_numbers_repeated
-                    sheet_data.loc[0:47, 'B'] = assigned_sample_ids_repeated
+                    sheet_data.loc[1:24, 13] = assigned_sample_ids
+                    sheet_data.loc[1:24, 14] = box_numbers_repeated
+                    sheet_data.loc[0:47, 1] = assigned_sample_ids_repeated
                 if sheet_name == '2 - Tops':
                     assigned_sample_ids_repeated = [sid for sid in assigned_sample_ids for _ in range(7)]
-                    sheet_data.loc[0:167, 'B'] = assigned_sample_ids_repeated
+                    sheet_data.loc[0:167, 1] = assigned_sample_ids_repeated
                 if sheet_name == '3 - Sides':
-                    sheet_data.loc[0:167, 'C'] = assigned_sample_ids_repeated
+                    sheet_data.loc[0:167, 2] = assigned_sample_ids_repeated
                 if sheet_name == '4 - 4.5 mL Tops':
-                    sheet_data.loc[0:23, 'C'] = assigned_sample_ids
+                    sheet_data.loc[0:23, 2] = assigned_sample_ids
                 if sheet_name == '5 - 4.5 mL Sides':
-                    sheet_data.loc[0:23, 'B'] = assigned_sample_ids
+                    sheet_data.loc[0:23, 1] = assigned_sample_ids
                     
             if print_type == 'STANDARD':
                 if 'READ ME' in sheet_name:
@@ -143,7 +143,7 @@ def generate_workbook(assigned_sample_ids, box_start, box_end, sheet_name, templ
                     sheet_data.to_excel(writer, sheet_name=sheet_name, index=False)
                 if '2-Box Side round1' in sheet_name:
                     sheet_data.loc[1:6, 7] = assigned_sample_ids_1
-                    sheet_data.loc[1:90, 'L'] = assigned_sample_ids_1_repeated
+                    sheet_data.loc[1:90, 14] = assigned_sample_ids_1_repeated
                     sheet_data[1] = sheet_data[7] + " " + sheet_data[11]
                 if '3-Box Top round2' in sheet_name:
                     assigned_sample_ids_2 = assigned_sample_ids[6:12]
@@ -169,23 +169,23 @@ def generate_workbook(assigned_sample_ids, box_start, box_end, sheet_name, templ
             
             if print_type == 'SERONETPBMC':
                 if 'Instructions' in sheet_name:
-                    sheet_data.loc[1:96, 'V'] = assigned_sample_ids
+                    sheet_data.loc[1:96, 21] = assigned_sample_ids
                     box_numbers = [box for box in range(box_start, box_end + 1) for _ in range(3)]
-                    sheet_data.loc[1:96, 'W'] = box_numbers
+                    sheet_data.loc[1:96, 22] = box_numbers
                 if 'PBMC Tops' in sheet_name:
-                    sheet_data.loc[0:95, 'B'] = assigned_sample_ids
+                    sheet_data.loc[0:95, 1] = assigned_sample_ids
                 if 'PBMC Sides' in sheet_name:
-                    sheet_data.loc[0:95, 'B'] = assigned_sample_ids
+                    sheet_data.loc[0:95, 1] = assigned_sample_ids
             
             elif print_type == 'STANDARDPBMC':
                 if 'Instructions' in sheet_name:
-                    sheet_data.loc[1:96, 'V'] = assigned_sample_ids
+                    sheet_data.loc[1:96, 21] = assigned_sample_ids
                     box_numbers = [box for box in range(box_start, box_end + 1) for _ in range(3)]
-                    sheet_data.loc[1:96, 'W'] = box_numbers
+                    sheet_data.loc[1:96, 22] = box_numbers
                 if 'PBMC Tops' in sheet_name:
-                    sheet_data.loc[0:95, 'B'] = assigned_sample_ids
+                    sheet_data.loc[0:95, 1] = assigned_sample_ids
                 if 'PBMC Sides' in sheet_name:
-                    sheet_data.loc[0:95, 'B'] = assigned_sample_ids
+                    sheet_data.loc[0:95, 1] = assigned_sample_ids
 
             sheet_data.to_excel(writer, sheet_name=sheet_name, index=False)
 
@@ -215,7 +215,7 @@ if __name__ == '__main__':
     assigned_sample_ids = get_sample_ids(sheet_name, box_start, box_end)
 
     # Output
-    workbook_name = f"{sheet_name.upper()} {'PBMC ' if 'PBMC' in print_type else ''}{box_start}-{box_end} test test"
+    workbook_name = f"{sheet_name.upper()} {'PBMC ' if 'PBMC' in print_type else ''}{box_start}-{box_end} test DO NOT USE"
     template_path = os.path.join(util.tube_print, 'Future Sheets', template_file)
     output_path = os.path.join(util.tube_print, output_folder, f"{workbook_name}.xlsx")
 
