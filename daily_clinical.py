9ca820aec28ad93320ce110c555a849c1dafb103
Charles Gleason <36055314+crgl@users.noreply.github.com>
2023-09-21 11:45:53 -0400

    Add last visit info to daily_clinical

--- daily_clinical.py
+++ daily_clinical.py
@@ -3,6 +3,7 @@ import pandas as pd
 import datetime
 import argparse
 import util
+from helpers import query_intake, query_dscf
 
 def seronet_dems(dem_cols):
     iris_data = util.iris_folder + 'IRIS for D4 Long.xlsx'
@@ -16,7 +17,7 @@ def seronet_dems(dem_cols):
         dfs.append(df.loc[:, dem_cols])
     return pd.concat(dfs)
 
-if __name__ == '__main__':
+def all_dems():
     dem_cols = ['Participant ID', 'Age', 'Sex_At_Birth', 'Race', 'Ethnicity', 'Height', 'Weight', 'BMI']
     paris_info = pd.read_excel(util.projects + 'PARIS/Demographics.xlsx').loc[:, ['Subject ID', 'Age at Baseline', 'NIH Race', 'NIH Ethnicity', 'NIH Sex']].rename(
         columns={'Subject ID': 'Participant ID',
@@ -43,6 +44,20 @@ if __name__ == '__main__':
     )
     dem_dfs = [paris_info, seronet_info, shield_info, umbrella_info, hd_info]
     output_df = pd.concat(dem_dfs).loc[:, dem_cols].fillna('Unknown').drop_duplicates(subset='Participant ID')
+    return output_df
+
+if __name__ == '__main__':
+    intake_df = query_intake(include_research=True).reset_index().sort_values(by=['participant_id', 'Date Collected'])
+    total_visits = intake_df.drop_duplicates(subset=['participant_id', 'Date Collected']).groupby('participant_id').count().iloc[:, :1]
+    total_visits.columns = ['Visit Count']
+    last_visit = intake_df.drop_duplicates(subset=['participant_id'], keep='last').set_index('participant_id').loc[:, ['Date Collected', 'sample_id']]
+    last_visit.columns = ['Date of Most Recent Visit', 'Most Recent Sample ID']
+    first_visit = intake_df.drop_duplicates(subset=['participant_id'], keep='first').set_index('participant_id').loc[:, ['Date Collected', 'sample_id']]
+    first_visit.columns = ['Baseline Date', 'Baseline Sample ID']
+    visit_info = total_visits.join(last_visit).join(first_visit)
+    visit_info['Days Followed'] = (visit_info['Date of Most Recent Visit'] - visit_info['Baseline Date']).dt.days + 1
+    visit_info['Visits per Week'] = 7 * visit_info['Visit Count'] / visit_info['Days Followed']
+    output_df = all_dems().set_index('Participant ID').join(visit_info).reset_index()
     hd = output_df[output_df['Participant ID'].astype(str).str.contains('16772')]
     hd_summary = pd.crosstab(index=hd['Race'], columns=[hd['Ethnicity'], hd['Sex_At_Birth']])
     umbrella = output_df[output_df['Participant ID'].astype(str).str.contains('16791')]
