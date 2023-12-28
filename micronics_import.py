e38428f3c0c76d07887c6abe7194c8bd64793069
YC <yxchen17@hotmail.com>
2023-12-28 11:33:29 -0500

    Refactor Transform Function
    Modify transform function to append data directly.

--- micronics_import.py
+++ micronics_import.py
@@ -11,27 +11,26 @@ def convert_tube_position(position):
     number = int(position[1:])
     return f"{number}/{letter}"
 
-def transform_sample_data(sheet, box_file_name):
+def transform_sample_data(sheet, box_file_name, data):
     '''Transforms each row of the scanned sheet'''
-    transformed_data = deepcopy(data)
     for idx, row in sheet.iterrows():
         tube_position = convert_tube_position(row['Tube Position'])
         box_name = box_file_name
         sample_id = str(row['Sample ID'])
-        transformed_data['Name'].append(sample_id)
-        transformed_data['Sample ID'].append(sample_id)
-        transformed_data['Sample Type'].append('Serum Micronic')
-        transformed_data['Volume'].append('0.4')
-        transformed_data['Freezer'].append('Annenberg 18')
-        transformed_data['Level1'].append('Freezer 1 (Eiffel Tower)')
-        transformed_data['Level2'].append('Shelf 4')
-        transformed_data['Level3'].append('Rack 2')
-        transformed_data['Box'].append(box_name)
-        transformed_data['Position'].append(tube_position)
-        transformed_data['ALIQUOT'].append(sample_id)
-        transformed_data['Barcode'].append(row['Tube ID'])
-        transformed_data['Original Plate Barcode'].append(row['Rack ID'])
-    return transformed_data
+        data['Name'].append(sample_id)
+        data['Sample ID'].append(sample_id)
+        data['Sample Type'].append('Serum Micronic')
+        data['Volume'].append('0.4')
+        data['Freezer'].append('Annenberg 18')
+        data['Level1'].append('Freezer 1 (Eiffel Tower)')
+        data['Level2'].append('Shelf 4')
+        data['Level3'].append('Rack 2')
+        data['Box'].append(box_name)
+        data['Position'].append(tube_position)
+        data['ALIQUOT'].append(sample_id)
+        data['Barcode'].append(row['Tube ID'])
+        data['Original Plate Barcode'].append(row['Rack ID'])
+    return data
 
 if __name__ == '__main__':
     argParser = argparse.ArgumentParser(description='Transform micronics data for FP upload')
@@ -52,9 +51,7 @@ if __name__ == '__main__':
         inventory_box = pd.read_excel(input_file, sheet_name=None)
 
         for name, sheet in inventory_box.items():
-            transformed_sample_data = transform_sample_data(sheet, box_file_name)
-            for key in data:
-                data[key].extend(transformed_sample_data[key])
+            transform_sample_data(sheet, box_file_name, data)
     
     output_file = os.path.join(util.project_ws, 'CRP aliquoting/CRP Micronics Files/', f'micronics_fp_upload {today_date}.xlsx')
     pd.DataFrame(data).to_excel(output_file, index=False)
