54c81aa72c6a04e43b79889fe57ee3675dabd3f2
Charles Gleason <36055314+crgl@users.noreply.github.com>
2024-03-06 12:55:34 -0500

    Add skeleton of missing_info notification script

--- proc_missing_info.py
+++ proc_missing_info.py
@@ -6,47 +6,26 @@ import util
 import helpers
 
 
-# Missing Information from Processing Notebook:
-
-## Pull filtered list from specimen dashboard into separate output seet
-## Notify individuals based on initials
-## Automate script to run every morning
-
-
-# Set path
 if __name__ == '__main__':
-    argParser = argparse.ArgumentParser(description='Make Seronet monthly sample report.')
-    argParser.add_argument('-m', '--min_count', action='store', type=int, default=78)
-    args = argParser.parse_args()
+    argparser = argparse.ArgumentParser(description='Assign blame. Benignly.')
+    argparser.add_argument('-r', '--recency', type=int, default=14, help='Number of days in the past to consider recent')
+    args = argparser.parse_args()
+
+    df_all = (pd.read_excel(util.proc_ntbk, sheet_name='Specimen Dashboard', header=1)
+                .assign(date=lambda df: df['Date Processed'].apply(helpers.coerce_date)))
+    df = df_all[(df_all['date'] >= datetime.today() - datetime.timedelta(days=args.recency))
+                 & (df_all['date'] < datetime.today())].copy()
     
-    df = (pd.read_excel(util.proc_ntbk, sheet_name='Specimen Dashboard', header=1)
-            .assign(date=lambda df: df['Date Processed'].apply(helpers.coerce_date)))
-    df['Date Processed'] = df['Date Processed'].apply(helpers.coerce_date)    # Change datatype to date
-    
-    # Set cutoff date for processing
-    cutoff_date = (datetime.datetime.now() - datetime.timedelta(days=3)).date()    # better set days=3 for Mon-lastFri = 3d
-
-    # Filter for samples: 3 days ago + missing data (sample_complete=No)
-    missing_df = df[(df['Date Processed'] <= cutoff_date) & (df['Sample Complete?'] == 'No')]
-
-    # Export filtered list
-    output_path = util.proc + 'Processing Missing Information.xlsx'
-    missing_df.to_excel(output_path, index=False)
-
-
-    # # Notify individuals based on initials
-    # outlook = win32.Dispatch('outlook.application')
-    # mail = outlook.CreateItem(0)
-
-    # initial_info = [('YC', 'yuexing.chen@mssm.edu', 'Yuexing'), ('', '', '')]
-    
-    # for initials, email, first_name in initial_info:
-    #     initials_missing_df = missing_df.loc[missing_df['Processor Initials'] == initials]
-    #     if len(initials_missing_df) > 0:
-    #         mail.To = email
-    #         mail.Subject = f"DO NOT REPLY: Missing Processing Information for Samples Processed on or before {cutoff_date}"
-    #         mail.HTMLBody = f"Good morning {first_name},<br><br>The following samples are missing processing information:<br><br>{initials_missing_df.to_html(index=False)}<br><br>Thank you,<br>Processing Team"
-    #         mail.Send()
-
-    print("Daily check is finished. Notification emails have been sent. Have a good day!")
-
+    init_cols = ['Processed by (initials)', 'Cells Processed By', 'Who Moved Samples to LN?',
+                 'Plasma Aliquoted by', 'Serum Aliquoted by', 'Saliva Aliquoted by']
+    df['any_responsibility'] = df.loc[:, init_cols].apply(lambda row: '; '.join(row.values.astype(str)), axis=1)
+    for col in init_cols:
+        df['Missing: ' + col] = df[col] == 0
+        df[col] = df[col].where(df[col] != 0, df['any_responsibility'])
+    init_info = pd.read_excel(util.proc + 'script_data/initial_mapping.xlsx').set_index('Initials')
+    future_output = {'Inits': [], 'Name': [], 'Sample ID': [], 'Sheet': [], 'Column': [], 'Current Value': [], 'Issue': [], 'Fixed': []}
+    issues = []
+    issue_inits = []
+    # for col
+    for issue, init_source in zip(issues, issue_inits):
+        pass
