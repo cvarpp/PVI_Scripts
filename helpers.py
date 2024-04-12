0d493cd4bc10182f6f81eeb1cadcc5f363d3ebff
MonahB01 <brian.monahan@mssm.edu>
2024-04-12 11:02:15 -0400

    Added new test ID to Idset for passwords

--- helpers.py
+++ helpers.py
@@ -5,7 +5,7 @@ import numpy as np
 import util
 import os
 
-idset=['0406c79a0b6ea2c8a31f150b21585d6c9ddab384a877db727010369906f53b1b']
+idset=['0406c79a0b6ea2c8a31f150b21585d6c9ddab384a877db727010369906f53b1b','eb807ee5a8682e082b6bacb74ed394e05cd7099404a92dd91aca7c7a3fbd3036']
 
 class ValuesToClass(object):
     def __init__(self,values):
@@ -290,6 +290,7 @@ def map_dates(df, date_cols):
     for col in date_cols:
         df[col] = df[col].apply(coerce_date)
     return df
+
 
 def corned_beef(userid, userkey):
     h = hlib.sha256()
