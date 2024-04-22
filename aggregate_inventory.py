6de6ea038a3bc1821828387aea75ac0d7a2089b7
ychen <yxchen17@hotmail.com>
2024-04-22 18:07:39 -0400

    Add NPS to aggregate_inventory.py (#34)
    * Add NPS to aggregate_inventory.py
    
    * Simplify processing and allow for new name format
    
    * Rename PSP NPS box names
    
    Rename the PSP NPS box names to the format 'PSP NPS Lab/FF #' without underscores.
    
    * Simplify code for team Assignment
    
    * Revert elif/if swap, simplify timp check
    
    * Align with 'PSP' team
    
    * Remove Redundant PSP Handling
    
    ---------
    
    Co-authored-by: Charles Gleason <36055314+crgl@users.noreply.github.com>

--- aggregate_inventory.py
+++ aggregate_inventory.py
@@ -6,7 +6,6 @@ from datetime import date
 import re
 from copy import deepcopy
 import os
-import pickle
 import argparse
 from helpers import clean_sample_id
 
@@ -26,23 +25,12 @@ def filter_box(box_name, uploaded, args, box_df=None):
         return False
     return (box_name not in uploaded) and ((args.min_count <= box_df.set_index('Name').loc[box_name, 'Tube Count'] <= 81) or (box_name in full_des))
 
-def timp_check(name):
-    '''
-    Returns True if the box is for a SERONET study
-    '''
-    mit = "MIT" in name.upper()
-    mars = "MARS" in name.upper()
-    iris = "IRIS" in name.upper()
-    titan = "TITAN" in name.upper()
-    prio = "PRIORITY" in name.upper()
-    return mit or mars or iris or titan or prio
-
 if __name__ == '__main__':
     argParser = argparse.ArgumentParser(description='Aggregate inventory of each sample type for FP upload')
     argParser.add_argument('-m', '--min_count', action='store', type=int, default=78)
     args = argParser.parse_args()
     inventory_boxes = pd.read_excel(util.inventory_input, sheet_name=None)
-    sample_types = ['Plasma', 'Serum', 'Pellet', 'Saliva', 'PBMC', 'HT', '4.5 mL Tube', 'All']
+    sample_types = ['Plasma', 'Serum', 'Pellet', 'Saliva', 'PBMC', 'HT', '4.5 mL Tube', 'NPS', 'All']
     data = {'Name': [], 'Sample ID': [], 'Sample Type': [],'Freezer': [],'Level1': [],'Level2': [],'Level3': [],'Box': [],'Position': [], 'ALIQUOT': []}
     samples_data = {st: deepcopy(data) for st in sample_types if st != 'HT'}
     samples_data['Serum']['Heat treated?'] = []
@@ -54,7 +42,7 @@ if __name__ == '__main__':
 
     for name, sheet in inventory_boxes.items():
         try:
-            box_number = int(name.split()[-1])
+            box_number = int(name.rstrip('LabFF_ ').split()[-1])
         except:
             print(name, "has no box number")
             continue
@@ -64,7 +52,7 @@ if __name__ == '__main__':
             team = 'APOLLO'
         elif re.search('PSP', name):
             team = 'PSP'
-        elif timp_check(name):
+        elif re.match("MIT|MARS|IRIS|TITAN|PRIORITY", name.upper()) is not None:
             team = 'PVI ' + name.split()[0]
         else:
             team = 'PVI'
@@ -79,20 +67,12 @@ if __name__ == '__main__':
         if sample_type == 'N/A':
             print(name, "has a box number but no valid sample type")
             continue
-        box_kinds = []
-        if re.search("RESEARCH", name.upper()):
-            box_kinds.append("RESEARCH")
-        if re.search("NIH", name.upper()):
-            box_kinds.append("NIH")
-        if re.search("LAB", name.upper()):
-            box_kinds.append("Lab")
-        if re.search("FF", name.upper()):
-            box_kinds.append("FF")
+        box_kinds = re.findall('RESEARCH|NIH|Lab|FF', name)
         if len(box_kinds) == 0:
             if sample_type in ['PBMC', 'HT', '4.5 mL Tube']:
                 box_kinds.append('')
             else:
-                print(name, " is neither lab nor ff")
+                print(name, " is neither lab nor ff. Not uploaded")
         box_sample_type = sample_type
         sheet = sheet.assign(sample_id=clean_sample_id).set_index('sample_id')
         for kind in box_kinds:
@@ -109,6 +89,11 @@ if __name__ == '__main__':
             level2 = 'Shelf 2'
             if team == 'APOLLO':
                 level3 = 'APOLLO Rack'
+            elif box_sample_type == 'NPS':
+                freezer = 'Temporary PSP NPS'
+                level1 = 'freezer_nps'
+                level2 = 'shelf_nps'
+                level3 = 'rack_nps'
             elif box_sample_type == 'Saliva' or box_sample_type == 'Pellet':
                 level3 = '{} Lab/FF Rack'.format(box_sample_type)
             elif box_sample_type == 'PBMC':
@@ -133,41 +118,26 @@ if __name__ == '__main__':
                 if sample_type == 'N/A':
                     print(row['Sample ID'], 'in box', box_name, 'has no sample type specified. (', row['Sample Type'], ')')
                     continue
-                data = samples_data[sample_type]
-                data['Name'].append(sample_id)
-                data['Sample ID'].append(sample_id)
-                data['Sample Type'].append(row['Sample Type'].strip())
-                data['Freezer'].append(freezer)
-                data['Level1'].append(level1)
-                data['Level2'].append(level2)
-                data['Level3'].append(level3)
-                data['Box'].append(box_name)
-                data['Position'].append(pos_convert(idx))
-                data['ALIQUOT'].append(sample_id)
-                if sample_type in ['Serum', 'Plasma']:
-                    if box_sample_type == 'HT':
-                        data['Heat treated?'].append('Yes')
-                    else:
-                        data['Heat treated?'].append('No')
-                if sample_type == '4.5 mL Tube':
-                    data['Serum or Plasma?'].append(row['Serum or Plasma?'])
-                data = samples_data['All']
-                data['Name'].append(sample_id)
-                data['Sample ID'].append(sample_id)
-                data['Sample Type'].append(row['Sample Type'].strip())
-                data['Freezer'].append(freezer)
-                data['Level1'].append(level1)
-                data['Level2'].append(level2)
-                data['Level3'].append(level3)
-                data['Box'].append(box_name)
-                data['Position'].append(pos_convert(idx))
-                data['ALIQUOT'].append(sample_id)
+                for sname in [sample_type, 'All']:
+                    data = samples_data[sname]
+                    data['Name'].append(sample_id)
+                    data['Sample ID'].append(sample_id)
+                    data['Sample Type'].append(row['Sample Type'].strip())
+                    data['Freezer'].append(freezer)
+                    data['Level1'].append(level1)
+                    data['Level2'].append(level2)
+                    data['Level3'].append(level3)
+                    data['Box'].append(box_name)
+                    data['Position'].append(pos_convert(idx))
+                    data['ALIQUOT'].append(sample_id)
+                    if sname in ['Serum', 'Plasma']:
+                        if box_sample_type == 'HT':
+                            data['Heat treated?'].append('Yes')
+                        else:
+                            data['Heat treated?'].append('No')
+                    if sname == '4.5 mL Tube':
+                        data['Serum or Plasma?'].append(row['Serum or Plasma?'])
                 box_counts[box_name] += 1
-    # if os.path.exists(processing + 'script_data/uploaded_boxes.pkl'):
-        # with open(processing + 'script_data/uploaded_boxes.pkl', 'rb') as f:
-            # uploaded = pickle.load(f)
-    # else:
-        # uploaded = set()
     uploaded = set()
     full_des = set(inventory_boxes['Full Boxes, DES']['Name'].to_numpy())
     box_data = {'Name': [], 'Tube Count': []}
@@ -192,5 +162,3 @@ if __name__ == '__main__':
     for _, row in uploading_boxes.iterrows():
             print(row['Name'])
             uploaded.add(row['Name'])
-    # with open(processing + 'script_data/uploaded_boxes.pkl', 'wb+') as f:
-        # pickle.dump(uploaded, f)
