11aa1a418da11bd1fc277da549dd7c2c6335fb75
Charles Gleason <36055314+crgl@users.noreply.github.com>
2024-05-01 17:49:30 -0400

    Add input file customization to general results

--- results/general.py
+++ results/general.py
@@ -7,13 +7,20 @@ from helpers import query_intake
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description='Generate report based on participant IDs in results_of_interest. Legacy script')
+    argparser.add_argument('-i', '--input_file', action='store', default=util.script_input + 'results_of_interest.xlsx', help="Path to input file (defaults to script input)")
     argparser.add_argument('-o', '--output_file', action='store', default='tmp', help="Prefix for the output file (in addition to current date)")
     argparser.add_argument('-d', '--debug', action='store_true', help="Print to the command line but do not write to file")
     args = argparser.parse_args()
 
-    input_filename = util.script_input + 'results_of_interest.xlsx'
-    print("List of participant ids used from {}".format(input_filename))
-    participants = [str(x).strip().upper() for x in pd.read_excel(input_filename)['Participant ID']]
+    print("List of participant ids used from {}".format(args.input_file))
+    if 'xls' in args.input_file:
+        pid_df = pd.read_excel(args.input_file)
+    elif 'csv' in args.input_file:
+        pid_df = pd.read_csv(args.input_file)
+    else:
+        print("Invalid file type. Fatal error, exiting...")
+        exit(1)
+    participants = [str(x).strip().upper() for x in pid_df['Participant ID']]
     print("Pulling data for {} participants".format(len(participants)))
     samplesClean = query_intake(participants=participants, include_research=True).rename(
         columns={'Date Collected': 'Date',
