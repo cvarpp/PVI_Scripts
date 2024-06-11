e022ad61d1148a7ce10d2feec75ab54f09a8ab8a
Charles Gleason <36055314+crgl@users.noreply.github.com>
2024-06-11 10:26:34 -0400

    Remove query_dscf script in favor of info pull

--- augments/query_dscf.py
+++
@@ -1,8 +0,0 @@
-from helpers import query_dscf
-import pandas as pd
-
-
-if __name__ == '__main__':
-    df = pd.read_excel('local_secrets/input.xlsx')
-    df2 = query_dscf(sid_list=df['Sample ID'].astype(str).str.strip().str.upper().unique())
-    df2.to_excel('local_secrets/proc_info.xlsx')
