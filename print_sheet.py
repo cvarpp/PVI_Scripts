80b451a90fa406416852663efda44882ab1333a4
ychen <yxchen17@hotmail.com>
2024-09-04 11:50:45 -0400

    Refactor print_sheet.py and add APOLLO Print Type (#37)
    * Refactor Iterative Process for SERONET and STANDARD
    
    Temporarily change output file name ending with TESTING.
    
    * Update SERONET PBMC Template Name
    
    * Add APOLLO to print_sheet.py
    
    * Remove TESTING from output file names
    
    Add accidentally deleted numpy package back
    
    * Clean up comments
    
    ---------
    
    Co-authored-by: Charles Gleason <36055314+crgl@users.noreply.github.com>

--- print_sheet.py
+++ print_sheet.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import argparse
 import util
 import os
@@ -6,7 +7,13 @@ import warnings
 
 
 def get_box_range(print_type, round_num):
-    # Rename Printing Log: Study - Kit Type
+    '''
+    Get the range of box numbers to print for a given kit type and round number
+    print_type: str
+    A valid type of kit to be printed
+    round_num: int
+    Non-negative integer representing how many rounds have already been selected
+    '''
     plog = (pd.read_excel(util.print_log, sheet_name='LOG', header=0)
             .rename(columns={'Box numbers': 'Box Min', 'Unnamed: 4': 'Box Max', 'Study': 'Kit Type'})
             .drop(1))
@@ -17,7 +24,8 @@ def get_box_range(print_type, round_num):
         'SERUM': (plog['Kit Type'] == 'SERUM'),
         'STANDARD': (plog['Kit Type'] == 'STANDARD') & (plog['PBMCs'].str.strip().str.lower() == 'no'),
         'SERONETPBMC': (plog['Kit Type'].isin(['SERONET', 'MIT (PBMCS)'])) & (plog['PBMCs'].str.strip().str.lower() == 'yes'),
-        'STANDARDPBMC': (plog['Kit Type'] == 'STANDARD') & (plog['PBMCs'].str.strip().str.lower() == 'yes')
+        'STANDARDPBMC': (plog['Kit Type'] == 'STANDARD') & (plog['PBMCs'].str.strip().str.lower() == 'yes'),
+        'APOLLO':(plog['Kit Type'] == 'APOLLO') & (plog['PBMCs'].str.strip().str.lower() == 'no')
     }
 
     if print_type not in filter:
@@ -33,6 +41,7 @@ def get_box_range(print_type, round_num):
         'STANDARD': 6,
         'SERONETPBMC': 32,
         'STANDARDPBMC': 32,
+        'APOLLO': 5
     }
     box_start = recent_box_max + 1 + round_num * box_range_mapping[print_type]
     box_end = recent_box_max + box_range_mapping[print_type] + round_num * box_range_mapping[print_type]
@@ -48,12 +57,11 @@ def get_sample_ids(sheet_name, box_start, box_end):
 
     return sample_ids
 
-
 def generate_workbook(assigned_sample_ids, box_start, box_end, sheet_name, template_folder, output_path, print_type):
     template_path = os.path.join(util.tube_print, 'Future Sheets', template_folder)
     template = pd.read_excel(template_path, sheet_name=None, header=None)
 
-    with pd.ExcelWriter(output_path, engine='xlsxwriter') as writer:
+    with pd.ExcelWriter(output_path) as writer:
         for sheet_name, sheet_data in template.items():
             
             if print_type == 'SERUM':
@@ -77,27 +85,22 @@ def generate_workbook(assigned_sample_ids, box_start, box_end, sheet_name, templ
                     sheet_data.iloc[7:25, 7] = assigned_sample_ids
                     sheet_data.iloc[1:19, 12] = assigned_sample_ids
                     sheet_data.iloc[1:19, 13] = [box for box in range(box_start, box_end + 1) for _ in range(3)]
-                if '1-Box Top round1' in sheet_name:
-                    sheet_data.iloc[:6, 7] = assigned_sample_ids[:6]
-                    sheet_data.iloc[:90, 2] = [sid for sid in assigned_sample_ids[:6] for _ in range(15)]
-                if '2-Box Side round1' in sheet_name:
-                    sheet_data.iloc[:6, 7] = assigned_sample_ids[:6]
-                    sheet_data.iloc[:90, 11] = [sid for sid in assigned_sample_ids[:6] for _ in range(15)]
-                    sheet_data[2] = sheet_data[12] + " " + sheet_data[11]
-                if '3-Box Top round2' in sheet_name:
-                    sheet_data.iloc[:6, 7] = assigned_sample_ids[6:12]
-                    sheet_data.iloc[:90, 2] = [sid for sid in assigned_sample_ids[6:12] for _ in range(15)]
-                if '4-Box Side round2' in sheet_name:
-                    sheet_data.iloc[:6, 7] = assigned_sample_ids[6:12]
-                    sheet_data.iloc[:90, 11] = [sid for sid in assigned_sample_ids[6:12] for _ in range(15)]
-                    sheet_data[2] = sheet_data[12] + " " + sheet_data[11]
-                if '5-Box Top round3' in sheet_name:
-                    sheet_data.iloc[:6, 7] = assigned_sample_ids[12:]
-                    sheet_data.iloc[:90, 2] = [sid for sid in assigned_sample_ids[12:] for _ in range(15)]
-                if '6-Box Side round3' in sheet_name:
-                    sheet_data.iloc[:6, 7] = assigned_sample_ids[12:]
-                    sheet_data.iloc[:90, 11] = [sid for sid in assigned_sample_ids[12:] for _ in range(15)]
-                    sheet_data[2] = sheet_data[12] + " " + sheet_data[11]
+                
+                for round_num in range(1, 4):
+                    top_sheet = f'{round_num * 2 - 1}-Box Top round{round_num}'
+                    side_sheet = f'{round_num * 2}-Box Side round{round_num}'
+                    idx_start = (round_num - 1) * 6
+                    idx_end = round_num * 6
+                    
+                    if top_sheet in sheet_name:
+                        sheet_data.iloc[:6, 7] = assigned_sample_ids[idx_start:idx_end]
+                        sheet_data.iloc[:90, 2] = [sid for sid in assigned_sample_ids[idx_start:idx_end] for _ in range(15)]
+                    
+                    if side_sheet in sheet_name:
+                        sheet_data.iloc[:6, 7] = assigned_sample_ids[idx_start:idx_end]
+                        sheet_data.iloc[:90, 11] = [sid for sid in assigned_sample_ids[idx_start:idx_end] for _ in range(15)]
+                        sheet_data[2] = sheet_data[12] + " " + sheet_data[11]
+                
                 if '7-Kits' in sheet_name:
                     sheet_data.iloc[:90, 1] = [sid for i in range(0, len(assigned_sample_ids), 2) for sid in [assigned_sample_ids[i], assigned_sample_ids[i + 1]] * 5]
 
@@ -128,8 +131,34 @@ def generate_workbook(assigned_sample_ids, box_start, box_end, sheet_name, templ
                     if 'PBMC Tops' in sheet_name or 'PBMC Sides' in sheet_name:
                         sheet_data.iloc[:288, 2] = [sid for sid in assigned_sample_ids for _ in range(3)]
 
-            sheet_data.to_excel(writer, sheet_name=sheet_name, index=False, header=False)
-
+            elif print_type == 'APOLLO':
+                sids = assigned_sample_ids
+                sid_kits = []
+                for sid in sids:
+                    sid_kits.extend([sid] * 5)
+                counts = list(np.concatenate([2 * np.arange(5) + 1, 2 * np.arange(5) + 2])) * 9
+                count_adders = np.arange(108, step=12)
+                count_adders2 = []
+                for count_adder in count_adders:
+                    count_adders2.extend([count_adder] * 10)
+                final_index = np.array(counts) + np.array(count_adders2)
+                kit_df = pd.DataFrame({'Column A': final_index, 'Sample ID': sid_kits, 'Kit Type': 'APOLLO'}).sort_values(by='Column A')
+                kit_df.to_excel(writer, sheet_name='1 - Kits', index=False, header=False)
+                sid_aliquots = []
+                for sid in sids:
+                    sid_aliquots.extend([sid] * 16)
+                for x in range(3):
+                    round_num = x + 1
+                    top_idx = round_num * 2
+                    side_idx = top_idx + 1
+                    local_sids = sid_aliquots[x * 16 * 6:(x + 1) * 16 * 6]
+                    top_index = np.concatenate([np.arange(48) + 1, np.arange(48) + 1 + 78])
+                    side_index = np.concatenate([np.arange(48) + 1, np.arange(48) + 1 + 50])
+                    top_df = pd.DataFrame({'Column A': top_index, 'Blank': '', 'Sample ID': local_sids, 'Sample Type': 'Serum'})
+                    top_df.to_excel(writer, sheet_name=f'{top_idx} - Tops {round_num}', index=False, header=False)
+                    side_writers = ['Serum {}'.format(sid) for sid in local_sids]
+                    side_df = pd.DataFrame({'Column A': side_index, 'Blank': '', 'Sample ID': side_writers, 'Kit Type': 'APOLLO'})
+                    side_df.to_excel(writer, sheet_name=f'{side_idx} - Sides {round_num}', index=False, header=False)
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
@@ -139,6 +168,7 @@ if __name__ == '__main__':
     parser.add_argument('-standard', type=int, default=0, help='Number of STANDARD rounds')
     parser.add_argument('-seronet_pbmc', type=int, default=0, help='Number of SERONET PBMC rounds')
     parser.add_argument('-standard_pbmc', type=int, default=0, help='Number of STANDARD PBMC rounds')
+    parser.add_argument('-apollo', type=int, default=0, help='Number of APOLLO rounds')
     args = parser.parse_args()
 
     round_counts = {
@@ -147,24 +177,24 @@ if __name__ == '__main__':
         'STANDARD': args.standard,
         'SERONETPBMC': args.seronet_pbmc,
         'STANDARDPBMC': args.standard_pbmc,
+        'APOLLO': args.apollo,
     }
 
     for print_type, rounds in round_counts.items():
-        for round_num in range(0, rounds):
-            # Box range
+        for round_num in range(rounds):
             box_start, box_end = get_box_range(print_type, round_num)
 
-            # Map print_type to workbook_name & template_file & output_path
+            # TODO: Remove dependence on templates
             print_type_mapping = {
                 'SERONET': ('Seronet Full', 'SERONET FULL/SERONET FULL Template.xlsx', 'Future Sheets/SERONET FULL'),
                 'SERUM': ('Serum', 'SERUM/SERUM Template.xlsx', 'Future Sheets/SERUM'),
                 'STANDARD': ('Standard', 'STANDARD/STANDARD Template.xlsx', 'Future Sheets/STANDARD'),
-                'SERONETPBMC': ('Seronet Full', 'SERONET FULL/SERONET FULL PBMC Template.xlsx', 'Future Sheets/SERONET FULL'),
-                'STANDARDPBMC': ('Standard', 'STANDARD/STANDARD PBMC Template.xlsx', 'Future Sheets/STANDARD')
+                'SERONETPBMC': ('Seronet Full', 'SERONET FULL/SERONET PBMC Template.xlsx', 'Future Sheets/SERONET FULL'),
+                'STANDARDPBMC': ('Standard', 'STANDARD/STANDARD PBMC Template.xlsx', 'Future Sheets/STANDARD'),
+                'APOLLO': ('APOLLO', 'APOLLO/APOLLO Template.xlsx', 'Future Sheets/APOLLO'),
             }
             sheet_name, template_file, output_folder = print_type_mapping[print_type]
 
-            # Sample IDs within box range
             assigned_sample_ids = get_sample_ids(sheet_name, box_start, box_end)
 
             if not assigned_sample_ids.any():
@@ -172,9 +202,10 @@ if __name__ == '__main__':
                 continue
             
             # Output
-            workbook_name = f"{sheet_name.upper()} {'PBMC ' if 'PBMC' in print_type else ''}{box_start}-{box_end} Round {round_num + 1} by scripts"
+            workbook_name = f"{sheet_name.upper()} {'PBMC ' if 'PBMC' in print_type else ''}{box_start}-{box_end} by scripts"
             template_path = os.path.join(util.tube_print, 'Future Sheets', template_file)
             output_path = os.path.join(util.tube_print, output_folder, f"{workbook_name}.xlsx")
 
             generate_workbook(assigned_sample_ids, box_start, box_end, workbook_name, template_path, output_path, print_type)
             print(f"'{workbook_name}' workbook generated in {output_folder}.")
+            
