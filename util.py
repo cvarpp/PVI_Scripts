8090f0a85c6ae27a71e44005a4fed0ab27ac2314
MonahB01 <107202736+MonahB01@users.noreply.github.com>
2024-10-04 17:51:42 -0400

    Update to util for work at home

--- util.py
+++ util.py
@@ -59,6 +59,10 @@ report_form = script_output + 'SERONET.csv'
 cross_project = clin_ops + 'Cross-Project/'
 sample_query = cross_project + 'Sample ID Query/'
 sharing = cross_project + 'Result Sharing/'
+
+RnD = cross_project + 'R&D/'
+Robin_dove = RnD + 'ROBIN & DOVE sample info.xlsx'
+
 cross_d4 = cross_project + 'Seronet Task D4/'
 seronet_data = cross_d4 + 'Data/'
 seronet_qc = cross_d4 + 'QC/'
