d58d06631db3a2d22856457cba6af56e2b45303e
Jessica Nardulli <131474194+narduj01@users.noreply.github.com>
2025-04-11 17:09:54 -0400

    join cohorts within each study
    i don't know if concat is the right function to use in line 118 but i don't think it should be join there too?

--- pvi_dashboard.py
+++ pvi_dashboard.py
@@ -23,7 +23,7 @@ with warnings.catch_warnings():
     apollo_infections.set_index('Participant ID', inplace=True)
     apollo_data = (apollo_data.join(apollo_vaccines, on='Participant ID', how = 'inner')
                               .join(apollo_infections, on='Participant ID', how = 'inner')
-                              .reset_index().copy())
+                              .copy())
     apollo_data['Study'] = 'APOLLO'
     apollo_data.rename(columns={'Age @ Enrollment':'Age'}, inplace=True)
 
@@ -37,7 +37,7 @@ with warnings.catch_warnings():
     paris_data.rename(columns={'Study Status':'Status'}, inplace=True)
     paris_data = (paris_data.join(paris_dems, on='Participant ID', rsuffix='_dem')
                             .join(paris_main, on='Participant ID', rsuffix='_main')
-                            .reset_index().copy())
+                            .copy())
     paris_data.rename(columns={'E-mail':'Email', 'Date of Birth':'DOB', 'Gender':'Sex', 'NIH Race':'Race', 'NIH Ethnicity':'Ethnicity'}, inplace=True)
     paris_data['Study'] = 'PARIS'
 
@@ -52,6 +52,7 @@ with warnings.catch_warnings():
     dove_data['Participant ID'] = dove_data['Participant ID'].apply(lambda val: val.strip().upper())
     participants=np.append(participants, dove_data['Participant ID'].unique())
     dove_data.set_index('Participant ID', inplace=True)
+    hd_data = hd_data.join(dove_data, rsuffix='_dove').copy()
 
     #UMBRELLA
     umb_data=pd.read_excel(util.umbrella_tracker, sheet_name='Summary').dropna(subset=['Subject ID'])
@@ -66,18 +67,21 @@ with warnings.catch_warnings():
     crp_data['Participant ID'] = crp_data['Participant ID'].apply(lambda val: val.strip().upper())
     participants=np.append(participants, crp_data['Participant ID'].unique())
     crp_data.set_index('Participant ID', inplace=True)
+    umb_data = umb_data.join(crp_data, rsuffix='_crp').copy()
     #ROBIN
     robin_data = pd.read_excel(util.umbrella + 'ROBIN/ROBIN Participant tracker.xlsx', sheet_name='Participant Info').dropna(subset=['Subject ID'])
     robin_data.rename(columns={'Subject ID':'Participant ID'}, inplace=True)
     robin_data['Participant ID'] = robin_data['Participant ID'].apply(lambda val: val.strip().upper())
     participants=np.append(participants, robin_data['Participant ID'].unique())
     robin_data.set_index('Participant ID', inplace=True)
+    umb_data = umb_data.join(robin_data, rsuffix='_robin').copy()
     #SHIELD
     shield_data = pd.read_excel(util.projects +'SHIELD/SHIELD tracker.xlsx', sheet_name='Summary').dropna(subset=['Participant ID'])
     shield_data['Participant ID'] = shield_data['Participant ID'].astype(str).apply(lambda val: val.strip().upper())
     shield_data=shield_data[shield_data['Participant ID'].str.startswith('1')]
     participants=np.append(participants, shield_data['Participant ID'].unique())
     shield_data.set_index('Participant ID', inplace=True)
+    umb_data = umb_data.join(shield_data, rsuffix='_shield').copy()
     #GAEA
     gaea_data=pd.read_excel(util.gaea_folder + 'GAEA Tracker.xlsx', sheet_name='Summary').dropna(subset=['Participant ID'])
     gaea_infections=pd.read_excel(util.gaea_folder + 'GAEA Tracker.xlsx', sheet_name='Infections').dropna(subset=['Participant ID'])
@@ -85,6 +89,7 @@ with warnings.catch_warnings():
     participants=np.append(participants, gaea_data['Participant ID'].unique())
     gaea_data.set_index('Participant ID', inplace=True)
     gaea_infections.set_index('Participant ID', inplace=True)
+    umb_data = umb_data.join(gaea_data, rsuffix='_gaea').copy()
     #MARS
     mars_data=pd.read_excel(util.mars_folder + 'MARS tracker.xlsx', sheet_name='Pt List').dropna(subset=['Participant ID'])
     mars_infections=pd.read_excel(util.mars_folder + 'MARS tracker.xlsx', sheet_name='COVID_Infections').dropna(subset=['Participant ID'])
@@ -92,26 +97,36 @@ with warnings.catch_warnings():
     participants=np.append(participants, mars_data['Participant ID'].unique())
     mars_data.set_index('Participant ID', inplace=True)
     mars_infections.set_index('Participant ID', inplace=True)
+    umb_data = umb_data.join(mars_data, rsuffix='_mars').copy()
     #IRIS
     iris_data = pd.read_excel(util.iris_folder + 'Participant Tracking - IRIS.xlsx', sheet_name='Main Project', header=4).dropna(subset=['Participant ID'])
     iris_data['Participant ID'] = iris_data['Participant ID'].apply(lambda val: val.strip().upper())
     participants=np.append(participants, iris_data['Participant ID'].unique())
     iris_data.set_index('Participant ID', inplace=True)
+    umb_data = umb_data.join(iris_data, rsuffix='_iris').copy()
     #TITAN
     titan_data = pd.read_excel(util.titan_tracker, sheet_name='Tracker',header=4).dropna(subset=['Umbrella Corresponding Participant ID'])
     titan_data.rename(columns={'Umbrella Corresponding Participant ID':'Participant ID'}, inplace=True)
     titan_data['Participant ID'] = titan_data['Participant ID'].apply(lambda val: val.strip().upper())
     participants=np.append(participants, titan_data['Participant ID'].unique())
     titan_data.set_index('Participant ID', inplace=True)
+    umb_data = umb_data.join(titan_data, rsuffix='_titan').copy()
 
-    all_participants = (pd.concat([apollo_data,paris_data,hd_data,dove_data,umb_data,crp_data,robin_data,
-                                   shield_data,gaea_data,mars_data,iris_data,titan_data]))
-    pt_cols = ['Participant ID', 'Name', 'Email', 'Study', 'Status', 'Date of Consent', 'Baseline Date', 'DOB', 'Age', 'Sex', 'Gender', 'Race', 'Ethnicity']
-    all_participants['Date of Consent', 'Baseline Date', 'DOB']=all_participants['Date of Consent', 'Baseline Date', 'DOB'].str.strftime('%m.%d%y')
-    output = all_participants.reset_index().loc[:, pt_cols]
+    all_studies = [apollo_data, paris_data, hd_data, umb_data]
+    for df in all_studies:
+        df = df.reset_index()
+    all_participants = pd.concat(all_studies, ignore_index=True)
+    dem_cols = ['Participant ID', 'Name', 'Email', 'Study', 'Status', 'Date of Consent', 'Baseline Date', 'DOB', 'Age', 'Sex', 'Gender', 'Race', 'Ethnicity']
+    dems = all_participants.reset_index().loc[:, dem_cols]
+
     output_filename = util.pvi + 'Secret Sheets/Jessica/PVI Dashboard Testing/PVI Sampling Dashboard_{}.xlsx'.format(date.today().strftime("%m.%d.%y"))
-    output.to_excel(output_filename, index=False)
-    print("Written to {}".format(output_filename))
+    with pd.ExcelWriter(output_filename) as writer:
+        dems.to_excel(writer, index = False, sheet_name='All Demographics')
+        apollo_data.to_excel(writer, sheet_name='All APOLLO')
+        paris_data.to_excel(writer, sheet_name='All PARIS')
+        hd_data.to_excel(writer, sheet_name='All Healthy Donors')
+        umb_data.to_excel(writer, sheet_name='All Umbrella')
+        print("Written to {}".format(output_filename))
 
 if __name__ == '__main__':
     pass
