0f251400dc91571b3f0a84896b918de9083d1383
Carter-williams <carter.williams@mssm.edu>
2025-04-24 12:21:23 -0400

    Document and update rate limit for fp_locs (#53)
    * Document and Update Rate Limit for fp_locs
    
    Documented code, removed rate limit thanks to the 1200/min F2B update, updated time predictions accordingly, added option to input user/pass manually when environment variables are not set up.
    
    * Update fp_locs.py
    
    Added error catching for searching sid_json for "included," throws key error for some samples. Option to input credentials manually now accepts the input it claims to accept. Removed temporary debugging code left in by mistake. Updated poem to adhere to meter better.
    
    * Update fp_locs.py
    
    Implemented desired looping functionality for getting manual credentials. Removed poem because although it was written during my lunch break, it gave the impression that I was not a hard worker.
    
    * Create FP_box_locator.py
    
    CLI script that returns a csv of boxes + their locations.
    
    * Remove commented line from FP box script
    
    * Allow csv input and fix location import
    
    * Clean up trailing newlines
    
    ---------
    
    Co-authored-by: Charles Gleason <36055314+crgl@users.noreply.github.com>

--- fp_locs.py
+++ fp_locs.py
@@ -1,70 +1,123 @@
 import numpy as np
 import pandas as pd
-import util
 import os
 import requests
-from time import sleep
 import argparse
 
+"""
+CLI script that locates samples in FreezerPro
+Input: An excel file with a column of sample IDs with header "Sample ID"
+Output: An excel file with data about the number and type of samples for each ID
+Args: -i for input, -o for output, -d for debug (optional)
+Example: fp_locs.py -i inputs.xlsx -o outputs.xlsx
+Works best with environment variables named FP_USER and FP_PASS containing nuclear launch codes
+
+To Do: Add detailed information about the location of each sample to the output file
+NOTES:
+-Refused to work until I installed pip-system-certs through pip despite already having it
+"""
+
+# Establish a table for convering FP numerical sample types to human readable form
 future_vial_info = {'ID': ['25', '26', '27', '28', '29', '32'], 'Name': ['Serum','Plasma','Pellet','Saliva','PBMC','4.5 mL Tube']}
 vial_info = pd.DataFrame(future_vial_info).set_index('ID')
+fp_url = "https://mssm-simonlab.FreezerPro.com/api/v2"
 
+# If script is being run directly...
 if __name__ == '__main__':
+
+    # Parse CLI arguments and store in 'args'
     argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     argparser.add_argument('-d', '--debug', action='store_true')
     argparser.add_argument('-i', '--input_fname', action='store', required=True)
     argparser.add_argument('-o', '--output_fname', action='store', required=True)
     args = argparser.parse_args()
-    soi = pd.read_excel(args.input_fname)
-    if 'Sample ID' in soi.columns:
-        soi['sample_id'] = soi['Sample ID'].astype(str).str.strip().str.upper()
-    sample_types = ['Plasma', 'Serum', 'Saliva', 'Pellet', 'PBMC', '4.5 mL Tube']
-    fp_user = os.environ[util.fp_user]
-    fp_pass = os.environ[util.fp_pass]
-    token_response = requests.post(f'{util.fp_url}/auth/login', json={'username': fp_user, 'password': fp_pass})
+
+    # Read input excel file into a pandas dataframe and store cleaned inputs in 'soi'
+    if args.input_fname[-3:] == 'csv':
+        soi = pd.read_csv(args.input_fname)['Sample ID'].astype(str).str.strip().str.upper()
+    elif args.input_fname[-4:].isin(['xlsx', 'xlsm', '.xls']):
+        soi = pd.read_excel(args.input_fname)['Sample ID'].astype(str).str.strip().str.upper()
+
+    # Authenticate using username and password, then store token
+    username = os.environ.get("FP_USER")
+    password = os.environ.get("FP_PASS")
+    if not username or not password:
+        print("FP_USER and FP_PASS env variables not found. Please update then restart your shell.")
+        manual_response = ""
+        username = ""
+        password = ""
+        while manual_response == "":
+            manual_response = input("Input username/password manually? Y/N: ").lower()
+            if(manual_response in ["y", "yes", "sure, why not", "ok", "fine", "yes please"]):
+                username = input("Input username: ")
+                password = input ("Input password: ")
+            elif(manual_response == 'n'):
+                continue
+            else:
+                manual_response = ""
+    token_response = requests.post(f'{fp_url}/auth/login', json={'username': username, 'password': password})
     if token_response.status_code != 200:
-        print("Failed authentication. Fatal error, exiting...")
+        print("Failed authentication. Fatal Error. Exiting...")
         exit(1)
     token = token_response.json()['data']['attributes']['token']
+
+    # Save token to json header for requesting sample data
     headers = {'Authorization': f'token {token}'}
+    # List appropriate vial types for requesting sample data
     vial_type_suffix = 'include=vials&fields[sample]=name,vials,sample_type&fields[vial]=position_display,barcode_tag,box'
+    # Make empty output lists
     cols = ['Sample ID', 'Sample Type', 'Vial ID']
     vial_cols = ['Vial ID', 'Barcode', 'Position', 'Box ID']
     future_sids = {col: [] for col in cols}
     future_vials = {col: [] for col in vial_cols}
-    print("Querying FP for {} samples... Estimated time {:.1f} minutes".format(soi.shape[0], soi.shape[0] * 0.3 / 60))
-    for sid in soi['sample_id'].to_numpy()[:10]:
-        sleep(0.3)
-        sid_response = requests.get(f'{util.fp_url}/samples?filter[name_eq]={sid}&{vial_type_suffix}', headers=headers)
+    # Estimate querying time
+    print("Querying FP for {} samples... Estimated time {:.0f}M {:.0f}S".format(soi.shape[0],
+                                                                                  soi.shape[0] * 0.15 // 60,
+                                                                                  soi.shape[0] * .15 % 60))
+
+    # Fetch data for each sample ID
+    for sid in soi.to_numpy():
+        sid_response = requests.get(f'{fp_url}/samples?filter[name_eq]={sid}&{vial_type_suffix}', headers=headers)
         if sid_response.status_code != 200:
             print("Failed to query sample ID {}.".format(sid))
         else:
+            # Parse and store the response for each successfully queried sample ID
             sid_json = sid_response.json()
             for sample_data in sid_json['data']:
-                assert sample_data['attributes']['name'] == sid
+                assert sample_data['attributes']['name'] == sid, "Sample ID returned differed from the query."
                 stype = sample_data['relationships']['sample_type']['data']['id']
                 if stype not in vial_info.index:
                     print(stype, "not in known sample types.")
                     continue
                 for vial in sample_data['relationships']['vials']['data']:
-                    future_sids['Sample ID'].append(sid)
-                    future_sids['Sample Type'].append(vial_info.loc[stype, 'Name'])
+                    future_sids['Sample ID'].append(sid) # Log the ID
+                    future_sids['Sample Type'].append(vial_info.loc[stype, 'Name']) # Lookup and log the sample type
                     future_sids['Vial ID'].append(vial['id'])
-            for vial_data in sid_json['included']:
-                if vial_data['type'] == 'sample_type':
-                    continue
-                future_vials['Vial ID'].append(vial_data['id'])
-                future_vials['Barcode'].append(vial_data['attributes']['barcode_tag'])
-                future_vials['Position'].append(vial_data['attributes']['position_display'])
-                future_vials['Box ID'].append(vial_data['relationships']['box']['data']['id'])
+            try:
+                for vial_data in sid_json['included']:
+                    if vial_data['type'] == 'sample_type': # Type is almost always vial
+                        continue
+                    future_vials['Vial ID'].append(vial_data['id'])
+                    future_vials['Barcode'].append(vial_data['attributes']['barcode_tag'])
+                    future_vials['Position'].append(vial_data['attributes']['position_display'])
+                    future_vials['Box ID'].append(vial_data['relationships']['box']['data']['id'])
+            except KeyError:
+                if args.debug:
+                    print(f"Sample {sid} failed to return box information.")
+
+    
+    # Convert all of the lists of dicts we made into pandas dataframes
     samples = pd.DataFrame(future_sids)
     vials = pd.DataFrame(future_vials).set_index('Vial ID')
     box_cols = ['ID', 'Box Name', 'Box Barcode', 'Container']
     future_boxes = {col: [] for col in box_cols}
-    print("Querying FP for {} boxes... Estimated time {:.1f} minutes".format(vials['Box ID'].unique().shape[0], vials['Box ID'].unique().shape[0] * 0.3 / 60))
+
+    # A similar process is done for each unique box ID found above
+    print("Querying FP for {} boxes... Estimated time {:.0f}M {:.0f}S".format(vials['Box ID'].unique().shape[0], 
+                                                                         vials['Box ID'].unique().shape[0] * 0.05 // 60,
+                                                                         vials['Box ID'].unique().shape[0] * 0.05 % 60)) # This is faster than fetching vials
     for box_id in vials['Box ID'].unique():
-        sleep(0.3)
-        box_response = requests.get(f'{util.fp_url}/boxes/{box_id}?include=container&fields[box]=name,barcode_tag&fields[container]=name,barcode_tag', headers=headers)
+        box_response = requests.get(f'{fp_url}/boxes/{box_id}?include=container&fields[box]=name,barcode_tag&fields[container]=name,barcode_tag', headers=headers)
         if box_response.status_code != 200:
             print("Failed to query box {}.".format(box_id))
             print(box_response)
@@ -74,6 +127,8 @@ if __name__ == '__main__':
             future_boxes['Box Name'].append(box_json['data']['attributes']['name'])
             future_boxes['Box Barcode'].append(box_json['data']['attributes']['barcode_tag'])
             future_boxes['Container'].append(box_json['included'][0]['attributes']['name'])
+
+    # And again convert our lists of dicts to dataframes
     boxes = pd.DataFrame(future_boxes).set_index('ID')
     output = samples.join(vials, on='Vial ID').join(boxes, on='Box ID').sort_values(by='Box Name', key=lambda s: s.astype(str).str.contains('FF'), kind='stable')
     big_boxes = output[~output['Position'].astype(str).str.contains('/')]
@@ -81,10 +136,13 @@ if __name__ == '__main__':
     typecounts = reg_boxes.loc[:, ['Sample ID', 'Sample Type', 'Vial ID']].groupby(['Sample ID', 'Sample Type']
                     ).count().unstack(level=-1).droplevel(0, axis=1)
 
+    # Write data to output excel file
     if not args.debug:
         with pd.ExcelWriter(args.output_fname) as writer:
             typecounts.fillna(0).to_excel(writer, sheet_name='Summary')
-            for stype in sample_types:
+            for stype in vial_info['Name'].unique():
                 filtered = reg_boxes[reg_boxes['Sample Type'] == stype]
-                filtered.to_excel(writer, sheet_name=stype, index=False)
-            big_boxes.to_excel(writer, sheet_name='Collaborator or Discarded', index=False)
+                if filtered.shape[0] > 0:
+                    filtered.to_excel(writer, sheet_name=stype, index=False)
+            if big_boxes.shape[0] > 0:
+                big_boxes.to_excel(writer, sheet_name='Collaborator or Discarded', index=False)
