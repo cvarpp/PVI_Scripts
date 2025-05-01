a870415902e798e309664c86c2c9ce3dc2e2bac9
Jessica Nardulli <131474194+narduj01@users.noreply.github.com>
2025-05-01 16:39:14 -0400

    added a sars-cov-2 specific tab to pvi_dashboard
    this tab will be great for immune history figures! so far just apollo and paris, more to come!

--- pvi_dashboard.py
+++ pvi_dashboard.py
@@ -25,8 +25,31 @@ with warnings.catch_warnings():
                               .join(apollo_infections, on='Participant ID', how = 'inner')
                               .copy())
     apollo_data['Study'] = 'APOLLO'
-    apollo_data.rename(columns={'Age @ Enrollment':'Age'}, inplace=True)
+    apollo_data.rename(columns={'Age @ Enrollment':'Age', 'Number of Vaccine Doses':'Total Vaccine Doses', 
+                                'Number of SARS-CoV-2 Infections':'Total Infections','Symptom Onset Date 1':'Infection 1 Date', 
+                                'Symptom Onset Date 2':'Infection 2 Date', 'Symptom Onset Date 3':'Infection 3 Date', 
+                                'Symptom Onset Date 4':'Infection 4 Date'}, inplace=True)
+    apollo_immune_event_cols = ['Dose #1 Date', 'Dose #2 Date', 'Dose #3 Date', 'Dose #4 Date', 'Dose #5 Date', 
+                     'Dose #6 Date', 'Dose #7 Date', 'Dose #8 Date', 'Dose #9 Date', 
+                     'Infection 1 Date', 'Infection 2 Date', 'Infection 3 Date', 'Infection 4 Date']
+    apollo_vaccine_date_cols = ['Dose #1 Date', 'Dose #2 Date', 'Dose #3 Date', 'Dose #4 Date', 'Dose #5 Date', 
+                     'Dose #6 Date', 'Dose #7 Date', 'Dose #8 Date', 'Dose #9 Date']
+    apollo_vaccine_type_cols = ['Dose #1 Type', 'Dose #2 Type', 'Dose #3 Type', 'Dose #4 Type', 'Dose #5 Type', 
+                     'Dose #6 Type', 'Dose #7 Type', 'Dose #8 Type', 'Dose #9 Type']
+    apollo_infection_date_cols = ['Infection 1 Date', 'Infection 2 Date', 'Infection 3 Date', 'Infection 4 Date']
+    apollo_data['Date of Last Immune Event'] = apollo_data.apply(lambda row: permissive_datemax([row[immune_event] for immune_event in apollo_immune_event_cols], pd.Timestamp.today() ), axis=1)
+    apollo_data['Immune History'] = apollo_data.apply(lambda row: immune_history(row[apollo_vaccine_date_cols], row[apollo_vaccine_type_cols], row[apollo_infection_date_cols], pd.Timestamp.today() ), axis=1)
+    apollo_data['Total Immune Events'] = apollo_data['Total Vaccine Doses'].astype(int) + apollo_data['Total Infections'].astype(int)
+    apollo_data['Last Immune Event'] = apollo_data['Immune History'].str.endswith('I').apply(lambda val: 'Infection' if val else 'Vaccination')
 
+    apollo_cols= ['Participant ID', 'Status', 'Date of Consent', 'Baseline Date', 'Baseline Sample ID', 'DOB', 'Age', 
+                  'Sex', 'Gender', 'Race', 'Ethnicity', 'Immune History', 'Total Immune Events', 'Total Vaccine Doses', 
+                  'Dose #1 Date', 'Dose #1 Type', 'Dose #2 Date', 'Dose #2 Type', 'Dose #3 Date', 'Dose #3 Type', 
+                  'Dose #4 Date', 'Dose #4 Type', 'Dose #5 Date', 'Dose #5 Type', 'Dose #6 Date', 'Dose #6 Type', 
+                  'Dose #7 Date', 'Dose #7 Type', 'Dose #8 Date', 'Dose #8 Type', 'Dose #9 Date', 'Dose #9 Type', 
+                  'Total Infections', 'Infection 1 Date', 'Infection 2 Date', 'Infection 3 Date', 'Infection 4 Date']
+    apollo_output = apollo_data.reset_index().loc[:, apollo_cols]
+    
     #PARIS
     paris_data = pd.read_excel(util.paris + 'Patient Tracking - PARIS.xlsx', sheet_name='Subgroups', header=4).dropna(subset=['Participant ID'])
     paris_main = pd.read_excel(util.paris + 'Patient Tracking - PARIS.xlsx', sheet_name='Main', header=8).dropna(subset=['Subject ID']).set_index('Subject ID')
@@ -38,14 +61,46 @@ with warnings.catch_warnings():
     paris_data = (paris_data.join(paris_dems, on='Participant ID', rsuffix='_dem')
                             .join(paris_main, on='Participant ID', rsuffix='_main')
                             .copy())
-    paris_data.rename(columns={'E-mail':'Email', 'Date of Birth':'DOB', 'Gender':'Sex'}, inplace=True)
+    paris_data.rename(columns={'E-mail_main':'Email', 'Date of Birth':'DOB', 'Gender':'Sex', 'Status':'Status_sg', 'Study Status':'Status',
+                               'Sample ID_main':'Baseline Sample ID', 'First Dose Date':'Dose #1 Date', 'Second Dose Date':'Dose #2 Date', 
+                               'Boost Date':'Dose #3 Date', 'Boost 2 Date':'Dose #4 Date', 'Boost 3 Date':'Dose #5 Date', 
+                               'Boost 4 Date':'Dose #6 Date', 'Boost 5 Date':'Dose #7 Date', 'Boost 6 Date':'Dose #8 Date',
+                               'Vaccine Type':'Dose #1 Type', 'Boost Type':'Dose #3 Type', 'Boost 2 Type':'Dose #4 Type', 'Boost 3 Type':'Dose #5 Type', 
+                               'Boost 4 Type':'Dose #6 Type', 'Boost 5 Type':'Dose #7 Type', 'Boost 6 Type':'Dose #8 Type'}, inplace=True)
+    paris_data['Dose #2 Type'] = paris_data['Dose #1 Type'].copy()
     paris_data['Study'] = 'PARIS'
+    paris_data['Ethnicity'] = paris_data['Ethnicity: Hispanic or Latino'].apply(lambda val: 'Hispanic or Latino' if val == 'Yes' else 'Not Hispanic or Latino')
+    paris_drop_cols = ['Unnamed: 0', 'Rought Last Name']
+    paris_immune_event_cols = ['Dose #1 Date', 'Dose #2 Date', 'Dose #3 Date', 'Dose #4 Date', 'Dose #5 Date', 
+                     'Dose #6 Date', 'Dose #7 Date', 'Dose #8 Date', 
+                     'Infection 1 Date', 'Infection 2 Date', 'Infection 3 Date', 'Infection 4 Date']
+    paris_vaccine_date_cols = ['Dose #1 Date', 'Dose #2 Date', 'Dose #3 Date', 'Dose #4 Date', 'Dose #5 Date', 
+                     'Dose #6 Date', 'Dose #7 Date', 'Dose #8 Date']
+    paris_vaccine_type_cols = ['Dose #1 Type', 'Dose #2 Type', 'Dose #3 Type', 'Dose #4 Type', 'Dose #5 Type', 
+                     'Dose #6 Type', 'Dose #7 Type', 'Dose #8 Type']
+    paris_infection_date_cols = ['Infection 1 Date', 'Infection 2 Date', 'Infection 3 Date', 'Infection 4 Date']
+    paris_data['Date of Last Immune Event'] = paris_data.apply(lambda row: permissive_datemax([row[immune_event] for immune_event in paris_immune_event_cols], pd.Timestamp.today()), axis=1)
+    paris_data['Immune History'] = paris_data.apply(lambda row: immune_history(row[paris_vaccine_date_cols], row[paris_vaccine_type_cols], row[paris_infection_date_cols], pd.Timestamp.today()), axis=1)
+    paris_data['Total Infections'] = paris_data['Immune History'].str.count('I')
+    paris_data['Total Immune Events'] = paris_data['Immune History'].str.count('-') + 1
+    paris_data['Total Vaccine Doses'] = paris_data['Total Immune Events'].astype(int) - paris_data['Total Infections'].astype(int)
+    paris_data['Last Immune Event'] = paris_data['Immune History'].str.endswith('I').apply(lambda val: 'Infection' if val else 'Vaccination')
+
+    paris_cols= ['Participant ID', 'Status', 'Date of Consent', 'Baseline Date', 'Baseline Sample ID', 'DOB', 'Age', 
+                  'Sex', 'Race', 'Ethnicity', 'Immune History', 'Total Immune Events', 'Total Vaccine Doses', 
+                  'Dose #1 Date', 'Dose #1 Type', 'Dose #2 Date', 'Dose #2 Type', 'Dose #3 Date', 'Dose #3 Type', 
+                  'Dose #4 Date', 'Dose #4 Type', 'Dose #5 Date', 'Dose #5 Type', 'Dose #6 Date', 'Dose #6 Type', 
+                  'Dose #7 Date', 'Dose #7 Type', 'Dose #8 Date', 'Dose #8 Type', 
+                  'Total Infections', 'Infection 1 Date', 'Infection 2 Date', 'Infection 3 Date', 'Infection 4 Date']
+    paris_output = paris_data.reset_index().loc[:, paris_cols]
 
     #HD
     hd_data = pd.read_excel(util.clin_ops + 'Healthy donors/Healthy Donors Participant Tracker.xlsx', sheet_name='Participants').dropna(subset=['Participant ID'])
     hd_data['Participant ID'] = hd_data['Participant ID'].apply(lambda val: val.strip().upper())
     participants=np.append(participants, hd_data['Participant ID'].unique())
     hd_data.set_index('Participant ID', inplace=True)
+    hd_data.rename(columns={'Date of consent':'Date of Consent', 'Age at Baseline':'Age'}, inplace=True)
+    hd_data['Baseline Date'] = hd_data['Date of Consent'].copy()
     hd_data['Study']='Healthy Donors'
     #DOVE
     dove_data=pd.read_excel(util.clin_ops + 'Healthy donors/DOVE/DOVE participant tracker.xlsx', sheet_name='Participant info').dropna(subset=['Participant ID'])
@@ -62,6 +117,7 @@ with warnings.catch_warnings():
     participants=np.append(participants, umb_data['Participant ID'].unique())
     umb_data.set_index('Participant ID', inplace=True)
     umb_vaccines.set_index('Participant ID', inplace=True)
+    umb_data.rename(columns={'Cohort':'Study', 'Study Status':'Status', 'Date of ICF':'Date of Consent', 'Baseline Visit Date':'Baseline Date'}, inplace=True)
     #CRP
     crp_data = pd.read_excel(util.crp_folder + 'CRP Patient Tracker no circ ref 7.7.23.xlsx', sheet_name='Tracker', header=4).dropna(subset=['Participant ID'])
     crp_data['Participant ID'] = crp_data['Participant ID'].apply(lambda val: val.strip().upper())
@@ -114,16 +170,23 @@ with warnings.catch_warnings():
 
     all_studies = [apollo_data, paris_data, hd_data, umb_data]
     all_participants = pd.concat(all_studies)
+
+    date_cols = ['Date of Consent', 'Baseline Date', 'DOB']
+
     dem_cols = ['Participant ID', 'Name', 'Email', 'Study', 'Status', 'Date of Consent', 'Baseline Date', 'DOB', 'Age', 'Sex', 'Gender', 'Race', 'Ethnicity']
     dems = all_participants.reset_index().loc[:, dem_cols]
 
+    covid_dfs=[apollo_output,paris_output]
+    all_covid = pd.concat(covid_dfs)
+
     output_filename = util.pvi + 'Secret Sheets/Jessica/PVI Dashboard Testing/PVI Sampling Dashboard_{}.xlsx'.format(date.today().strftime("%m.%d.%y"))
     with pd.ExcelWriter(output_filename) as writer:
-        dems.to_excel(writer, index = False, sheet_name='All Demographics')
-        apollo_data.to_excel(writer, sheet_name='All APOLLO')
-        paris_data.to_excel(writer, sheet_name='All PARIS')
-        hd_data.to_excel(writer, sheet_name='All Healthy Donors')
-        umb_data.to_excel(writer, sheet_name='All Umbrella')
+        dems.to_excel(writer, index = False, sheet_name='All Demographics', freeze_panes=(1,1))
+        all_covid.to_excel(writer, index = False, sheet_name='All SARS-CoV-2', freeze_panes=(1,1))
+        apollo_data.to_excel(writer, sheet_name='All APOLLO', freeze_panes=(1,1))
+        paris_data.to_excel(writer, sheet_name='All PARIS', freeze_panes=(1,1))
+        hd_data.to_excel(writer, sheet_name='All Healthy Donors', freeze_panes=(1,1))
+        umb_data.to_excel(writer, sheet_name='All Umbrella', freeze_panes=(1,1))
         print("Written to {}".format(output_filename))
 
 if __name__ == '__main__':
